9ea9bb980127afc160a6688f2fc83abf08bc84e4
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-10-21 01:42:43 +0200

    Create simple ThemeProvider and Default Theme

---
+++ src/DefaultTheme.js
@@ -0,0 +1,17 @@
+// @flow
+
+import {
+  indigo500,
+  indigo700,
+  pinkA200
+} from './styles/colors';
+
+const DefaultTheme = () => {
+    return {
+      primaryColor: indigo500,
+      primaryDarkColor: indigo700,
+      accentColor: pinkA200,
+    };
+};
+
+export default DefaultTheme;
