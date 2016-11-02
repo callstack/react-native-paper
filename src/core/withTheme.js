a32d2c2f4e5ed5d8cc9c34f5e64efae9ccc98370
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-11-02 06:54:54 +0530

    fix: copy static properties in HOC

--- src/core/withTheme.js
+++ src/core/withTheme.js
@@ -7,13 +7,8 @@ import React, {
 import { theme } from './ThemeProvider';
 
 export default function withTheme<T>(Comp: ReactClass<T>): ReactClass<T> {
-
-  const name = Comp.displayName || Comp.name;
-
-  return class extends Component {
-
-    static displayName = `Themed${name}`;
-
+  class ThemedComponent extends Component {
+    static displayName = `Themed${Comp.displayName || Comp.name}`;
     static contextTypes = {
       [theme]: PropTypes.object,
     };
@@ -21,5 +16,23 @@ export default function withTheme<T>(Comp: ReactClass<T>): ReactClass<T> {
     render() {
       return <Comp theme={this.context[theme]} {...this.props} />;
     }
-  };
+  }
+
+  // This is ugly, but we need to hoist static properties manually
+  for (const prop in Comp) {
+    if (prop !== 'displayName' && prop !== 'contextTypes') {
+      if (prop === 'propTypes') {
+        // Only the underlying component will receive the theme prop
+        // eslint-disable-next-line no-shadow, no-unused-vars
+        const { theme, ...propTypes } = Comp[prop];
+        /* $FlowFixMe */
+        ThemedComponent[prop] = propTypes;
+      } else {
+        /* $FlowFixMe */
+        ThemedComponent[prop] = Comp[prop];
+      }
+    }
+  }
+
+  return ThemedComponent;
 }
