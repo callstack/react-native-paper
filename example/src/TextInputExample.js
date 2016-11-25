811d3fba47ef2e31c928999542a3683b11ab01f0
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-11-26 00:03:33 +0200

    feat: add TextInput component

---
+++ example/src/TextInputExample.js
@@ -0,0 +1,43 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Colors, TextInput } from 'react-native-paper';
+
+export default class TextInputExample extends Component {
+  static title = 'TextInput';
+
+  state = {
+    text: '',
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <TextInput
+          style={styles.inputContainerStyle}
+          label="Normal input"
+          placeholder="Type something"
+          value={this.state.text}
+          onChangeText={text => this.setState({ text })}
+        />
+        <TextInput
+          disabled
+          style={styles.inputContainerStyle}
+          label="Disabled Input"
+        />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    padding: 8,
+  },
+  inputContainerStyle: {
+    margin: 8,
+  },
+});
