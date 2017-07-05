201702408fb8b9edb93e0b9bbcb9b13812f6171c
Raúl Gómez Acuña <rauliyohmc@users.noreply.github.com>
2017-07-05 19:11:04 +0200

    chore: add Git Hook to validate commit messages (#118)

---
+++ scripts/validateCommitMessage.js
@@ -0,0 +1,63 @@
+/* @flow */
+
+const fs = require('fs');
+const util = require('util');
+
+const MAX_LENGTH = 100;
+const PATTERN = /^(?:fixup!\s*)?(\w*)(\(([\w\$\.\*/-]*)\))?\: (.*)$/;
+const TYPES = {
+  feat: true,
+  fix: true,
+  docs: true,
+  style: true,
+  refactor: true,
+  perf: true,
+  test: true,
+  chore: true,
+  revert: true,
+  breaking: true,
+};
+
+function printError() {
+  console.error('INVALID COMMIT MSG: ' + util.format.apply(null, arguments));
+}
+
+function validateMessage(message /*: string */) {
+  if (message.length > MAX_LENGTH) {
+    printError('is longer than %d characters !', MAX_LENGTH);
+    return false;
+  }
+
+  const match = PATTERN.exec(message);
+
+  if (!match) {
+    printError('does not match "<type>(<scope>): <subject>" ! was: ' + message);
+    return false;
+  }
+  const type = match[1];
+
+  if (!TYPES.hasOwnProperty(type)) {
+    printError('"%s" is not allowed type !', type);
+    return false;
+  }
+
+  return true;
+}
+
+function firstLineFromBuffer(buffer /*: Buffer */) {
+  return buffer.toString().split('\n').shift();
+}
+
+const commitMsgFile = process.argv[2];
+
+try {
+  const buffer = fs.readFileSync(commitMsgFile);
+  const msg = firstLineFromBuffer(buffer);
+  if (!validateMessage(msg)) {
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
+} catch (error) {
+  process.exit(1);
+}
