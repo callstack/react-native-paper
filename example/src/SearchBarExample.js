07cf8bc98934623a2e321b0903b232a7777f3ff0
ferrannp <fnp.developer@gmail.com>
2017-07-06 12:50:24 +0200

    feat: SearchBar - Add the possibility to customize the left icon as clickable button.

--- example/src/SearchBarExample.js
+++ example/src/SearchBarExample.js
@@ -1,24 +1,49 @@
 /* @flow */
 
 import React, { Component } from 'react';
-import { SearchBar } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Caption, SearchBar } from 'react-native-paper';
 
 export default class SearchExample extends Component {
-  static title = 'Searchbar';
+  static title = 'SearchBar';
 
   state = {
-    query: '',
+    firstQuery: '',
+    secondQuery: '',
+    thirdQuery: '',
   };
-
-  _handleChangeSearch = query => this.setState({ query });
 
   render() {
     return (
-      <SearchBar
-        placeholder="Search"
-        onChangeText={this._handleChangeSearch}
-        value={this.state.query}
-      />
+      <View>
+        <SearchBar
+          placeholder="Search"
+          onChangeText={query => this.setState({ firstQuery: query })}
+          value={this.state.firstQuery}
+        />
+        <Caption style={styles.caption}>Clickable icon</Caption>
+        <SearchBar
+          placeholder="Search"
+          onChangeText={query => this.setState({ secondQuery: query })}
+          value={this.state.secondQuery}
+          onIconPress={() => this.props.navigation.goBack()}
+          iconName="arrow-back"
+        />
+        <SearchBar
+          placeholder="Search"
+          onChangeText={query => this.setState({ thirdQuery: query })}
+          value={this.state.thirdQuery}
+          onIconPress={() => {}}
+          iconName="menu"
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  caption: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+});
