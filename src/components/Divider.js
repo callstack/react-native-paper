f9cab2f97904d12b0aa8e41fa87f60afe787d64f
lukewalczak <lukasz.walczak@callstack.io>
2017-10-16 15:56:43 +0200

    docs: documented divider

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -11,6 +11,18 @@ type Props = {
 
 /**
  * A divider is a thin, lightweight rule that groups content in lists and page layouts
+ *
+ * **Usage:**
+ * ```
+ * const MyComponent = () => (
+ *   <View>
+ *     <Text>Apple</Text>
+ *     </Divider>
+ *     <Text>Orange</Text>
+ *     </Divider>
+ *   </Button>
+ * );
+ * ```
  */
 const Divider = (props: Props) => {
   const { inset, style } = props;
