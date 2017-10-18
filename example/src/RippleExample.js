ab4bda3fbeffaeba426f1be58750c782513579ec
lukewalczak <lukasz.walczak@callstack.io>
2017-10-18 16:30:54 +0200

    feat: add ripple example

--- example/src/RippleExample.js
+++ example/src/RippleExample.js
@@ -1,11 +1,36 @@
 /* @flow */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, StyleSheet } from 'react-native';
+import { TouchableRipple, withTheme, Paragraph } from 'react-native-paper';
 
-import { Component } from 'react';
-
-export default class RippleExample extends Component {
+class RippleExample extends Component {
   static title = 'Ripples';
+  static propTypes = {
+    theme: PropTypes.object.isRequired,
+  };
 
   render() {
-    return null;
+    const { theme: { colors: { background } } } = this.props;
+    return (
+      <TouchableRipple
+        style={[styles.container, { backgroundColor: background }]}
+        onPress={() => {}}
+      >
+        <View>
+          <Paragraph>Press me</Paragraph>
+        </View>
+      </TouchableRipple>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default withTheme(RippleExample);
