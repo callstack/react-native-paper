f75e004a453a7696b21acfa4340aa3afee369bf8
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-11-06 14:31:35 +0530

    chore: configure circle ci (#202)
    Replace TravisCI with CircleCI for consistency across all callstack repos

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
+
 # Based on domenic's tutorial: https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+
 set -e # Exit with nonzero exit code if anything fails
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-function doCompile {
-  npm run build
-}
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
+cd $DIR
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -22,7 +17,7 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into dist/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
 git clone $REPO dist
 cd dist
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
@@ -31,12 +26,13 @@ cd ..
 # Clean dist existing contents
 rm -rf dist/**/* || exit 0
 
-# Run our compile script
-doCompile
+# Run our build script.
+yarn run build
 
-# Now let's go have some fun with the cloned repo
 cd dist
-git config user.name "Travis CI"
+
+# Configure git.
+git config user.name "$COMMIT_AUTHOR_NAME"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled dist (e.g. this is a README update) then just bail.
@@ -49,16 +45,6 @@ fi
 # The delta will show diffs between new and old versions.
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
