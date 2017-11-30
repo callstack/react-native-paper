0d8e590191e9382efd15bf2bd6c1c58c97f10992
Circle CI <gh-bot@callstack.com>
2017-11-30 13:22:55 +0000

    Deploy to GitHub Pages: b0d49c7815924a716355e1cac36845110015f98b

--- app.src.js
+++ app.src.js
@@ -4,10 +4,9 @@ import ReactDOM from 'react-dom';
 import RedBox from 'redbox-react';
 import App from '/home/circleci/react-native-paper/docs/node_modules/component-docs/dist/templates/App.js';
 import Layout from '/home/circleci/react-native-paper/docs/node_modules/component-docs/dist/templates/Layout.js';
-import data from './app.data.json';
+import data from './app.data';
 import '/home/circleci/react-native-paper/docs/node_modules/component-docs/dist/styles/reset.css';
 import '/home/circleci/react-native-paper/docs/node_modules/component-docs/dist/styles/globals.css';
-import '/home/circleci/react-native-paper/docs/node_modules/component-docs/dist/styles.css';
 
 const root = document.getElementById('root');
 const render = () => {
