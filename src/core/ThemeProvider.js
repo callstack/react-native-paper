8cf22bf7be2338a85055e1c47655a0e9e7c5b3a2
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-03-03 14:22:19 +0100

    refactor: use create-react-context for theming

--- src/core/ThemeProvider.js
+++ src/core/ThemeProvider.js
@@ -1,59 +1,27 @@
 /* @flow */
 
 import * as React from 'react';
-import PropTypes from 'prop-types';
+import createReactContext, { type Context } from 'create-react-context';
 import DefaultTheme from '../styles/DefaultTheme';
 import type { Theme } from '../types';
 
 type Props = {
   children: React.Node,
-  theme?: Theme,
+  theme: Theme,
 };
 
-export const channel = 'react-native-paper$theme';
+export const ThemeContext: Context<Theme> = createReactContext(DefaultTheme);
 
 export default class ThemeProvider extends React.Component<Props> {
   static defaultProps = {
     theme: DefaultTheme,
   };
 
-  static childContextTypes = {
-    [channel]: PropTypes.object,
-  };
-
-  getChildContext() {
-    return {
-      [channel]: {
-        subscribe: this._subscribe,
-        get: this._get,
-      },
-    };
-  }
-
-  componentWillReceiveProps(nextProps: *) {
-    if (this.props.theme !== nextProps.theme) {
-      this._subscriptions.forEach(cb => cb(nextProps.theme));
-    }
-  }
-
-  _subscriptions = [];
-
-  _subscribe = (callback: Theme => void) => {
-    this._subscriptions.push(callback);
-
-    const remove = () => {
-      const index = this._subscriptions.indexOf(callback);
-      if (index > -1) {
-        this._subscriptions.splice(index, 1);
-      }
-    };
-
-    return { remove };
-  };
-
-  _get = () => this.props.theme;
-
   render() {
-    return this.props.children;
+    return (
+      <ThemeContext.Provider value={this.props.theme}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
   }
 }
