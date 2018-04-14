2b94926bb9a1fe74a96ebdf44b48b4aace39080e
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-04-14 02:05:41 +0200

    feat: add babel plugin for modular imports

---
+++ src/babel/__fixtures__/rewrite-imports/code.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import {
+  Provider as PaperProvider,
+  BottomNavigation,
+  Button,
+  CardCover,
+  Toolbar,
+  NonExistent,
+  NonExistentSecond as Stuff,
+} from 'react-native-paper';
