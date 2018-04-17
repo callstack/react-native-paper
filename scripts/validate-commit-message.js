b544a42e2d1b5ef10492fc6487d82568ef1a83c4
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-04-17 13:59:38 +0200

    feat: add babel plugin for modular imports (#309)

--- scripts/validate-commit-message.js
+++ scripts/validate-commit-message.js
@@ -4,40 +4,41 @@ const fs = require('fs');
 const util = require('util');
 
 const MAX_LENGTH = 100;
-const PATTERN = /^(?:fixup!\s*)?(\w*)(\(([\w\$\.\*/-]*)\))?\: (.*)$/;
-const TYPES = {
-  feat: true,
-  fix: true,
-  docs: true,
-  style: true,
-  refactor: true,
-  perf: true,
-  test: true,
-  chore: true,
-  revert: true,
-  BREAKING: true
-};
+const PATTERN = /^(?:fixup!\s*)?(\w*)(\(([\w$.*/-]*)\))?: (.*)$/;
+const TYPES = [
+  'feat',
+  'fix',
+  'docs',
+  'style',
+  'refactor',
+  'perf',
+  'test',
+  'chore',
+  'revert',
+  'BREAKING',
+];
 
 function printError(...args) {
-  console.error('INVALID COMMIT MSG: ' + util.format.apply(null, args));
+  // eslint-disable-next-line no-console
+  console.error(`INVALID COMMIT MESSSAGE: ${util.format.apply(null, args)}`);
 }
 
 function validateMessage(message) {
   if (message.length > MAX_LENGTH) {
-    printError('is longer than %d characters !', MAX_LENGTH);
+    printError('is longer than %d characters!', MAX_LENGTH);
     return false;
   }
 
   const match = PATTERN.exec(message);
 
   if (!match) {
-    printError('does not match "<type>(<scope>): <subject>" ! was: ' + message);
+    printError(`does not match "<type>: <subject>"! was: ${message}`);
     return false;
   }
   const type = match[1];
 
-  if (!TYPES.hasOwnProperty(type)) {
-    printError('"%s" is not allowed type !', type);
+  if (!TYPES.includes(type)) {
+    printError('"%s" is not an allowed type!', type);
     return false;
   }
 
@@ -45,7 +46,10 @@ function validateMessage(message) {
 }
 
 function firstLineFromBuffer(buffer) {
-  return buffer.toString().split('\n').shift();
+  return buffer
+    .toString()
+    .split('\n')
+    .shift();
 }
 
 const commitMsgFile = process.argv[2];
---
+++ src/babel/__fixtures__/rewrite-imports/output.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+import PaperProvider from 'react-native-paper/src/core/Provider';
+import BottomNavigation from 'react-native-paper/src/components/BottomNavigation';
+import Button from 'react-native-paper/src/components/Button';
+import CardCover from 'react-native-paper/src/components/Card/CardCover';
+import Toolbar from 'react-native-paper/src/components/Toolbar/Toolbar';
+import { NonExistent, NonExistentSecond as Stuff } from 'react-native-paper';
