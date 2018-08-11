7a7de3074857357051cc5726c5997316f2c7dde5
Julian Hundeloh <jaulz@users.noreply.github.com>
2018-08-11 11:18:13 +0200

    fix: disable Touchable if no press handler was passed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -116,6 +116,7 @@ class Card extends React.Component<Props, State> {
       <AnimatedSurface style={[{ borderRadius: roundness, elevation }, style]}>
         <TouchableWithoutFeedback
           delayPressIn={0}
+          disabled={!onPress}
           onPress={onPress}
           onPressIn={onPress ? this._handlePressIn : undefined}
           onPressOut={onPress ? this._handlePressOut : undefined}
