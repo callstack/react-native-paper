2a61f3c27448c974cb71d14e57243205df01e070
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-09-02 20:24:12 +0200

    chore: update branding in the documentation

--- docs/pages/src/Home.js
+++ docs/pages/src/Home.js
@@ -16,7 +16,7 @@ export default class Home extends React.Component<{}> {
         <div {...styles(cover)}>
           <img
             {...styles(logo)}
-            src="images/paper-color.png"
+            src="images/paper-logo.svg"
             alt="React Native Paper"
           />
           <p>Cross-platform Material Design for React Native</p>
@@ -63,7 +63,7 @@ const logo = css`
 `;
 
 const elevated = css`
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.08), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.08), 0 3px 6px rgba(0, 0, 0, 0.24);
 `;
 
 const container = css`
@@ -93,40 +93,47 @@ const button = css`
   margin: 8px;
   min-width: 120px;
   font-size: 13px;
+  font-weight: 600;
   text-align: center;
   text-transform: uppercase;
-  padding: 9px 16px;
-  border: 0;
+  letter-spacing: 1px;
+  padding: 8px 16px;
+  border-width: 1px;
+  border-style: solid;
   border-radius: 3px;
   cursor: pointer;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12);
   transition: 0.3s;
 
   &:hover {
-    ${include(elevated)};
+    text-decoration: none;
   }
 `;
 
 const primary = css`
-  background-color: #3748ac;
+  background-color: #6200ee;
+  border-color: #6200ee;
   color: #fff;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12);
 
   &:hover,
   &:focus,
   &:active {
+    ${include(elevated)};
+
     color: #fff;
   }
 `;
 
 const secondary = css`
-  background-color: #fafafa;
-  color: #222;
-  font-weight: 600;
+  background-color: transparent;
+  border-color: rgba(0, 0, 0, 0.24);
+  color: #6200ee;
 
   &:hover,
   &:focus,
   &:active {
-    color: #222;
+    background-color: rgba(98, 0, 238, 0.08);
+    color: #6200ee;
   }
 `;
 
@@ -154,7 +161,7 @@ const screenshots = [
   'gallery/input.png',
   'gallery/searchbar.png',
   'gallery/typography.png',
-  'gallery/checkbox.png',
-  'gallery/radio.png',
-  'gallery/switch.png',
+  'gallery/chip.png',
+  'gallery/bottom-navigation.png',
+  'gallery/fab.png',
 ];
