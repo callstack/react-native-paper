b0ae62dd54664005031896a9c9b2dc672b4b52a2
Circle CI <gh-bot@callstack.com>
2018-09-04 11:50:24 +0000

    Deploy to GitHub Pages: 6785992d8f1c3003e213a6aab33fd1b579cbd93a

--- scripts/snack.js
+++ scripts/snack.js
@@ -10,47 +10,45 @@ function appendSnackLink() {
   if (usage) {
     var pre = usage.nextElementSibling;
 
-    while (
-      pre.firstChild.tagName !== 'CODE' ||
-      pre.firstChild.className !== 'language-js'
+    if (
+      pre &&
+      pre.firstChild.tagName === 'CODE' &&
+      pre.firstChild.className === 'language-js'
     ) {
-      pre = pre.nextElementSibling;
-    }
+      var link;
+      var insert = true;
 
-    if (!pre) {
-      return;
-    }
+      if (pre.nextElementSibling && pre.nextElementSibling.dataset.snack) {
+        link = pre.nextElementSibling;
+        insert = false;
+      } else {
+        link = document.createElement('a');
+        link.dataset.snack = true;
+        link.target = '_blank';
+        link.innerHTML =
+          'Try this example on Snack <svg width="14px" height="14px" viewBox="0 0 16 16" style="vertical-align: -1px"><g stroke="none" stroke-width="1" fill="none"><polyline stroke="currentColor" points="8.5 0.5 15.5 0.5 15.5 7.5"></polyline><path d="M8,8 L15.0710678,0.928932188" stroke="currentColor"></path><polyline stroke="currentColor" points="9.06944444 3.5 1.5 3.5 1.5 14.5 12.5 14.5 12.5 6.93055556"></polyline></g></svg>';
+      }
 
-    var link;
-    var insert = true;
+      var heading = document.querySelector('h1');
+      var href =
+        'https://snack.expo.io?name=' +
+        encodeURIComponent(
+          heading ? heading.textContent : document.title + ' Example'
+        ) +
+        '&description=' +
+        encodeURIComponent(window.location.href) +
+        '&code=' +
+        encodeURIComponent(pre.textContent);
 
-    if (pre.nextElementSibling && pre.nextElementSibling.dataset.snack) {
-      link = pre.nextElementSibling;
-      insert = false;
-    } else {
-      link = document.createElement('a');
-      link.dataset.snack = true;
-      link.target = '_blank';
-      link.innerHTML =
-        'Try this example on Snack <svg width="14px" height="14px" viewBox="0 0 16 16" style="vertical-align: -1px"><g stroke="none" stroke-width="1" fill="none"><polyline stroke="currentColor" points="8.5 0.5 15.5 0.5 15.5 7.5"></polyline><path d="M8,8 L15.0710678,0.928932188" stroke="currentColor"></path><polyline stroke="currentColor" points="9.06944444 3.5 1.5 3.5 1.5 14.5 12.5 14.5 12.5 6.93055556"></polyline></g></svg>';
-    }
+      if (link.href === href) {
+        return;
+      }
 
-    var href =
-      'https://snack.expo.io?name=' +
-      encodeURIComponent(document.title + ' Example') +
-      '&description=' +
-      encodeURIComponent(window.location.href) +
-      '&code=' +
-      encodeURIComponent(pre.textContent);
+      link.href = href;
 
-    if (link.href === href) {
-      return;
-    }
-
-    link.href = href;
-
-    if (insert) {
-      pre.insertAdjacentElement('afterend', link);
+      if (insert) {
+        pre.insertAdjacentElement('afterend', link);
+      }
     }
   }
 }
