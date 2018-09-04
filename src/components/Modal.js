callstack/react-native-paper

commit 7b0cd50daca34a1ed3327b47b7ef43f23cac535a

fix: fix issue that `Modal` children cannot grow (#491)
Add `flex:1` to Modal to fix the issue that `Modal` children cannot grow. fixes #471

src/components/Modal.js
@@ -154,7 +154,7 @@ class Modal extends React.Component<Props, State> {
             <View style={StyleSheet.absoluteFill} />
           </TouchableWithoutFeedback>
         )}
-        <Animated.View style={[{ opacity: this.state.opacity }]}>
+        <Animated.View style={[{ opacity: this.state.opacity }, styles.childrenWrapper]}>
           {children}
         </Animated.View>
       </Animated.View>
@@ -171,4 +171,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
   },
+  childrenWrapper: {
+    flex: 1
+  }
 });

commit 1b6e652e7fea1a928a9cd1e70ba3201d1b4c7e59

fix: fix dismissable prop of modal which broke in 7b0cd50

src/components/Modal.js
@@ -148,24 +148,18 @@ class Modal extends React.Component<Props, State> {
       <Animated.View
         accessibilityViewIsModal
         accessibilityLiveRegion="polite"
-        style={[{ opacity: this.state.opacity }, styles.wrapper]}
+        style={[{ opacity: this.state.opacity }, StyleSheet.absoluteFill]}
       >
-        <View
-          style={[
-            StyleSheet.absoluteFill,
-            { backgroundColor: colors.backdrop },
-          ]}
-        />
-        {dismissable && (
-          <TouchableWithoutFeedback onPress={this._hideModal}>
-            <View style={StyleSheet.absoluteFill} />
-          </TouchableWithoutFeedback>
-        )}
-        <Animated.View
-          style={[{ opacity: this.state.opacity }, styles.childrenWrapper]}
+        <TouchableWithoutFeedback
+          onPress={dismissable ? this._hideModal : undefined}
         >
+          <View
+            style={[styles.backdrop, { backgroundColor: colors.backdrop }]}
+          />
+        </TouchableWithoutFeedback>
+        <View pointerEvents="box-none" style={styles.content}>
           {children}
-        </Animated.View>
+        </View>
       </Animated.View>
     );
   }
@@ -176,11 +170,11 @@ polyfill(Modal);
 export default withTheme(Modal);
 
 const styles = StyleSheet.create({
-  wrapper: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  childrenWrapper: {
+  backdrop: {
     flex: 1,
+  },
+  content: {
+    ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
   },
 });