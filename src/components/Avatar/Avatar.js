71a87cc62b402dc5f609ed42daa0a0b2b54e32bd
nahoc-petal <ccarpentier@petalmd.com>
2018-09-10 12:38:31 -0400

    feat: Badge component

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,10 @@
+/* @flow */
+
+// @component ./AvatarIcon.js
+export { default as Icon } from './AvatarIcon';
+
+// @component ./AvatarImage.js
+export { default as Image } from './AvatarImage';
+
+// @component ./AvatarText.js
+export { default as Text } from './AvatarText';
