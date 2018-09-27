40e2bf4d07ede97e6da840c0fd5d9d44e55b91e9
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2018-09-27 22:45:17 +0200

    feat: Add typings for Banner component

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,6 +4,7 @@ import * as Drawer from './components/Drawer';
 
 export * from './types';
 export * from './components/Appbar';
+export * from './components/Banner';
 export * from './components/BottomNavigation';
 export * from './components/Typography';
 export * from './components/Button';
