224d473e514a4fc03e5bc57302e1fbd705b964dd
Dawid <dawidu@onet.pl>
2018-10-03 11:52:55 +0200

    docs: improve docs for fab component by adding styles. (#568)
    * docs: improve docs for fab component by adding styles.
    
    * refactor: remove unecessary white space.
    
    * refactor: improve fab docs.

--- src/components/FAB/FAB.js
+++ src/components/FAB/FAB.js
@@ -61,15 +61,26 @@ type Props = {
  * ## Usage
  * ```js
  * import * as React from 'react';
+ * import { StyleSheet } from 'react-native';
  * import { FAB } from 'react-native-paper';
  *
  * const MyComponent = () => (
  *   <FAB
+ *     style={styles.fab}
  *     small
  *     icon="add"
  *     onPress={() => console.log('Pressed')}
  *   />
  * );
+ *
+ * const styles = StyleSheet.create({
+ *   fab: {
+ *     position: 'absolute',
+ *     margin: 16,
+ *     right: 0,
+ *     bottom: 0,
+ *   },
+ * })
  *
  * export default MyComponent;
  * ```
