0f3d8c51577b2ff4b3a00c42cf30da820a568041
Altaf <ashaikh@mobiquityinc.com>
2018-10-11 18:59:08 +0530

    BREAKING: use correct props in radio button dialoge

--- example/src/Dialogs/DialogWithRadioBtns.js
+++ example/src/Dialogs/DialogWithRadioBtns.js
@@ -22,7 +22,7 @@ type State = {
 
 export default class extends React.Component<Props, State> {
   state = {
-    checked: 0,
+    checked: 0
   };
 
   render() {
@@ -38,7 +38,7 @@ export default class extends React.Component<Props, State> {
                 <TouchableRipple onPress={() => this.setState({ checked: 0 })}>
                   <View style={styles.row}>
                     <View pointerEvents="none">
-                      <RadioButton value="normal" checked={checked === 0} />
+                      <RadioButton value="normal" status={checked === 0 ? 'checked' : 'unchecked'} />
                     </View>
                     <Subheading style={styles.text}>Option 1</Subheading>
                   </View>
@@ -46,7 +46,7 @@ export default class extends React.Component<Props, State> {
                 <TouchableRipple onPress={() => this.setState({ checked: 1 })}>
                   <View style={styles.row}>
                     <View pointerEvents="none">
-                      <RadioButton value="normal" checked={checked === 1} />
+                      <RadioButton value="normal" status={checked === 1 ? 'checked' : 'unchecked'} />
                     </View>
                     <Subheading style={styles.text}>Option 2</Subheading>
                   </View>
@@ -54,7 +54,7 @@ export default class extends React.Component<Props, State> {
                 <TouchableRipple onPress={() => this.setState({ checked: 2 })}>
                   <View style={styles.row}>
                     <View pointerEvents="none">
-                      <RadioButton value="normal" checked={checked === 2} />
+                      <RadioButton value="normal" status={checked === 2 ? 'checked' : 'unchecked'} />
                     </View>
                     <Subheading style={styles.text}>Option 3</Subheading>
                   </View>
@@ -62,7 +62,7 @@ export default class extends React.Component<Props, State> {
                 <TouchableRipple onPress={() => this.setState({ checked: 3 })}>
                   <View style={styles.row}>
                     <View pointerEvents="none">
-                      <RadioButton value="normal" checked={checked === 3} />
+                      <RadioButton value="normal" status={checked === 3 ? 'checked' : 'unchecked'} />
                     </View>
                     <Subheading style={styles.text}>Option 4</Subheading>
                   </View>
