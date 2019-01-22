d969f021c6d734a34cba0e07c782592c46e5d99d
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-01-22 11:57:34 +0100

    fix: make type optional for helper text in TS definitions

--- typings/components/HelperText.d.ts
+++ typings/components/HelperText.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ThemeShape } from '../types';
 
 export interface HelperTextProps {
-  type: 'error' | 'info';
+  type?: 'error' | 'info';
   visible?: boolean;
   children: React.ReactNode;
   style?: any;
