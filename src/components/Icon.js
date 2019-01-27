df9e35ebb082a687a7e34b44b4eee20db719f1cb
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-01-27 15:42:58 +0100

    Update Icon.js

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -10,6 +10,7 @@ try {
   // Optionally require vector-icons
   MaterialIcons = require('react-native-vector-icons/MaterialIcons').default;
 } catch (e) {
+  // eslint-disable-next-line no-console
   console.error(e)
 
   if (global.__expo && global.__expo.Icon && global.__expo.Icon.MaterialIcons) {
@@ -21,7 +22,8 @@ try {
     MaterialIcons = ({ name, color, size, ...rest }) => {
       // eslint-disable-next-line no-console
       console.warn(
-        `Tried to use the icon '${name}' in a component from 'react-native-paper', but 'react-native-vector-icons' is not installed. To remove this warning, install 'react-native-vector-icons' or use another method to specify icon: https://callstack.github.io/react-native-paper/icons.html.`
+        `Tried to use the icon '${name}' in a component from 'react-native-paper', but 'react-native-vector-icons' could not be loaded.`,
+        `To remove this warning, try installing 'react-native-vector-icons' or use another method to specify icon: https://callstack.github.io/react-native-paper/icons.html.`		
       );
 
       return (
