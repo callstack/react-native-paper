cfcd102cdde314de99084c1c4954a7313c672e8f
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-01-27 09:40:46 -0500

    revised warn

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -21,7 +21,7 @@ try {
     MaterialIcons = ({ name, color, size, ...rest }) => {
       // eslint-disable-next-line no-console
       console.warn(
-        `Tried to use the icon '${name}' in a component from 'react-native-paper', but 'react-native-vector-icons' is not installed. To remove this warning, install 'react-native-vector-icons' or use another method to specify icon: https://callstack.github.io/react-native-paper/icons.html.`		
+        `Tried to use the icon '${name}' in a component from 'react-native-paper', but 'react-native-vector-icons' is not installed. To remove this warning, install 'react-native-vector-icons' or use another method to specify icon: https://callstack.github.io/react-native-paper/icons.html.`
       );
 
       return (
