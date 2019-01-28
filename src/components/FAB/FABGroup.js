b799a1e5f8e59e7afab1a04a2da6eabaad07f98a
Grzegorz Gawrysiak <grzegorz@appsolutions.pl>
2019-01-28 12:13:43 +0100

    feat: allow to change background for FAB Group (#758)
    ### Motivation
    
    It's currently possible to change the background color for FAB and also items within FAB.Group components. Changing the background color in FAB Group causes the overlay background to change.
    
    This PR makes it possible to change the background for the FAB Group main button.
    
    Current behavior:
    <img width="377" alt="screen shot 2019-01-25 at 17 41 56" src="https://user-images.githubusercontent.com/7827311/51761486-797b0280-20cd-11e9-93de-1eaf1ba64ff8.png">
    
    ### Test plan
    
    https://snack.expo.io/ryjNEaO7E

--- src/components/FAB/FABGroup.js
+++ src/components/FAB/FABGroup.js
@@ -72,6 +72,10 @@ type Props = {|
    */
   style?: any,
   /**
+   * Style for the FAB. It allows to pass the FAB button styles, such as backgroundColor.
+   */
+  fabStyle?: any,
+  /**
    * @optional
    */
   theme: Theme,
@@ -198,6 +202,7 @@ class FABGroup extends React.Component<Props, State> {
       accessibilityLabel,
       theme,
       style,
+      fabStyle,
       visible,
     } = this.props;
     const { colors } = theme;
@@ -312,7 +317,7 @@ class FABGroup extends React.Component<Props, State> {
           accessibilityTraits="button"
           accessibilityComponentType="button"
           accessibilityRole="button"
-          style={styles.fab}
+          style={[styles.fab, fabStyle]}
           visible={visible}
         />
       </View>
