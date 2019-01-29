6d1ab0d1aa6e9364da8c3a90dfd41f9815465ba1
jaulz <jhundeloh@yahoo.de>
2019-01-29 08:05:36 +0100

    fix: improve fonts on web

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -2,10 +2,12 @@ import { Platform } from 'react-native';
 
 const fonts = Platform.select({
   web: {
-    regular: 'Roboto',
-    medium: 'Roboto-Medium',
-    light: 'Roboto-Light',
-    thin: 'Roboto-Thin',
+    regular: 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
+    medium:
+      'Roboto-Medium, Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
+    light:
+      'Roboto-Light, Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
+    thin: 'Roboto-Thin, Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
   },
   ios: {
     regular: 'Helvetica Neue',
