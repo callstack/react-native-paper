1e70dd4ee7810874239257b4068933eac4207afd
Richard Lindhout <richardlindhout96@gmail.com>
2019-03-02 18:30:53 +0100

    fix: change banner test

--- src/components/__tests__/Banner.test.js
+++ src/components/__tests__/Banner.test.js
@@ -1,97 +1,695 @@
-/* @flow */
+// Jest Snapshot v1, https://goo.gl/fbAQLP
 
-import * as React from 'react';
-import renderer from 'react-test-renderer';
-import { Image } from 'react-native';
-import Banner from '../Banner';
-
-jest.useFakeTimers();
-
-it('renders hidden banner, without action buttons and without image', () => {
-  const tree = renderer
-    .create(
-      <Banner visible={false} actions={[]}>
-        Two line text string with two actions. One to two lines is preferable on
-        mobile.
-      </Banner>
-    )
-    .toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders visible banner, without action buttons and without image', () => {
-  const tree = renderer
-    .create(
-      <Banner visible actions={[]}>
-        Two line text string with two actions. One to two lines is preferable on
-        mobile.
-      </Banner>
-    )
-    .toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders visible banner, with action buttons and without image', () => {
-  const tree = renderer
-    .create(
-      <Banner
-        visible
-        actions={[
-          { label: 'first', onPress: () => {} },
-          { label: 'second', onPress: () => {} },
-        ]}
+exports[`renders hidden banner, without action buttons and without image 1`] = `
+<View
+  style={
+    Object {
+      "backgroundColor": "#ffffff",
+      "elevation": 1,
+      "shadowColor": "#000000",
+      "shadowOffset": Object {
+        "height": 0.5,
+        "width": 0,
+      },
+      "shadowOpacity": 0.24,
+      "shadowRadius": 0.75,
+    }
+  }
+>
+  <View
+    style={
+      Object {
+        "height": 0,
+      }
+    }
+  />
+  <View
+    onLayout={[Function]}
+    style={
+      Object {
+        "opacity": 0,
+        "position": "absolute",
+        "top": 0,
+        "transform": Array [
+          Object {
+            "translateY": -0,
+          },
+        ],
+        "width": "100%",
+      }
+    }
+  >
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-start",
+          "marginBottom": 0,
+          "marginHorizontal": 8,
+          "marginTop": 16,
+        }
+      }
+    >
+      <Text
+        style={
+          Array [
+            Object {
+              "color": "#000000",
+              "fontFamily": "Helvetica Neue",
+              "textAlign": "left",
+              "writingDirection": "ltr",
+            },
+            Object {
+              "flex": 1,
+              "margin": 8,
+            },
+          ]
+        }
       >
-        Two line text string with two actions. One to two lines is preferable on
-        mobile.
-      </Banner>
-    )
-    .toJSON();
+        Two line text string with two actions. One to two lines is preferable on mobile.
+      </Text>
+    </View>
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-end",
+          "margin": 4,
+        }
+      }
+    />
+  </View>
+</View>
+`;
 
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders visible banner, without action buttons and with image', () => {
-  const tree = renderer
-    .create(
-      <Banner
-        visible
-        image={({ size }) => (
-          <Image
-            source={{ uri: 'https://callstack.com/images/team/Satya.png' }}
-            style={{ width: size, height: size }}
-          />
-        )}
-        actions={[]}
+exports[`renders visible banner, with action buttons and with image 1`] = `
+<View
+  style={
+    Object {
+      "backgroundColor": "#ffffff",
+      "elevation": 1,
+      "shadowColor": "#000000",
+      "shadowOffset": Object {
+        "height": 0.5,
+        "width": 0,
+      },
+      "shadowOpacity": 0.24,
+      "shadowRadius": 0.75,
+    }
+  }
+>
+  <View
+    style={
+      Object {
+        "height": 0,
+      }
+    }
+  />
+  <View
+    onLayout={[Function]}
+    style={Object {}}
+  >
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-start",
+          "marginBottom": 0,
+          "marginHorizontal": 8,
+          "marginTop": 16,
+        }
+      }
+    >
+      <View
+        style={
+          Object {
+            "margin": 8,
+          }
+        }
       >
-        Two line text string with two actions. One to two lines is preferable on
-        mobile.
-      </Banner>
-    )
-    .toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders visible banner, with action buttons and with image', () => {
-  const tree = renderer
-    .create(
-      <Banner
-        visible
-        image={({ size }) => (
-          <Image
-            source={{ uri: 'https://callstack.com/images/team/Satya.png' }}
-            style={{ width: size, height: size }}
-          />
-        )}
-        actions={[{ label: 'first', onPress: () => {} }]}
+        <Image
+          source={
+            Object {
+              "uri": "https://callstack.com/images/team/Satya.png",
+            }
+          }
+          style={
+            Object {
+              "height": 40,
+              "width": 40,
+            }
+          }
+        />
+      </View>
+      <Text
+        style={
+          Array [
+            Object {
+              "color": "#000000",
+              "fontFamily": "Helvetica Neue",
+              "textAlign": "left",
+              "writingDirection": "ltr",
+            },
+            Object {
+              "flex": 1,
+              "margin": 8,
+            },
+          ]
+        }
       >
-        Two line text string with two actions. One to two lines is preferable on
-        mobile.
-      </Banner>
-    )
-    .toJSON();
+        Two line text string with two actions. One to two lines is preferable on mobile.
+      </Text>
+    </View>
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-end",
+          "margin": 4,
+        }
+      }
+    >
+      <View
+        style={
+          Object {
+            "backgroundColor": "transparent",
+            "borderColor": "transparent",
+            "borderRadius": 4,
+            "borderStyle": "solid",
+            "borderWidth": 0,
+            "elevation": 0,
+            "margin": 4,
+            "minWidth": "auto",
+            "shadowColor": "#000000",
+            "shadowOffset": Object {
+              "height": 0,
+              "width": 0,
+            },
+            "shadowOpacity": 0.24,
+            "shadowRadius": 0,
+          }
+        }
+      >
+        <View
+          accessibilityRole="button"
+          accessible={true}
+          isTVSelectable={true}
+          onResponderGrant={[Function]}
+          onResponderMove={[Function]}
+          onResponderRelease={[Function]}
+          onResponderTerminate={[Function]}
+          onResponderTerminationRequest={[Function]}
+          onStartShouldSetResponder={[Function]}
+          style={
+            Array [
+              Object {
+                "overflow": "hidden",
+              },
+              Array [
+                Object {
+                  "flex": 1,
+                  "width": "100%",
+                },
+                Object {
+                  "borderRadius": 4,
+                },
+              ],
+            ]
+          }
+        >
+          <View
+            style={
+              Array [
+                Object {
+                  "alignItems": "center",
+                  "flexDirection": "row",
+                  "justifyContent": "center",
+                },
+                undefined,
+              ]
+            }
+          >
+            <Text
+              numberOfLines={1}
+              style={
+                Array [
+                  Object {
+                    "color": "#000000",
+                    "fontFamily": "Helvetica Neue",
+                    "textAlign": "left",
+                    "writingDirection": "ltr",
+                  },
+                  Array [
+                    Object {
+                      "letterSpacing": 1,
+                      "marginHorizontal": 16,
+                      "marginVertical": 9,
+                      "textAlign": "center",
+                    },
+                    Object {
+                      "marginHorizontal": 8,
+                    },
+                    Object {
+                      "color": "#6200ee",
+                      "fontFamily": "HelveticaNeue-Medium",
+                    },
+                    Object {
+                      "fontFamily": "HelveticaNeue-Medium",
+                    },
+                  ],
+                ]
+              }
+            >
+              FIRST
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  </View>
+</View>
+`;
 
-  expect(tree).toMatchSnapshot();
-});
+exports[`renders visible banner, with action buttons and without image 1`] = `
+<View
+  style={
+    Object {
+      "backgroundColor": "#ffffff",
+      "elevation": 1,
+      "shadowColor": "#000000",
+      "shadowOffset": Object {
+        "height": 0.5,
+        "width": 0,
+      },
+      "shadowOpacity": 0.24,
+      "shadowRadius": 0.75,
+    }
+  }
+>
+  <View
+    style={
+      Object {
+        "height": 0,
+      }
+    }
+  />
+  <View
+    onLayout={[Function]}
+    style={Object {}}
+  >
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-start",
+          "marginBottom": 0,
+          "marginHorizontal": 8,
+          "marginTop": 16,
+        }
+      }
+    >
+      <Text
+        style={
+          Array [
+            Object {
+              "color": "#000000",
+              "fontFamily": "Helvetica Neue",
+              "textAlign": "left",
+              "writingDirection": "ltr",
+            },
+            Object {
+              "flex": 1,
+              "margin": 8,
+            },
+          ]
+        }
+      >
+        Two line text string with two actions. One to two lines is preferable on mobile.
+      </Text>
+    </View>
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-end",
+          "margin": 4,
+        }
+      }
+    >
+      <View
+        style={
+          Object {
+            "backgroundColor": "transparent",
+            "borderColor": "transparent",
+            "borderRadius": 4,
+            "borderStyle": "solid",
+            "borderWidth": 0,
+            "elevation": 0,
+            "margin": 4,
+            "minWidth": "auto",
+            "shadowColor": "#000000",
+            "shadowOffset": Object {
+              "height": 0,
+              "width": 0,
+            },
+            "shadowOpacity": 0.24,
+            "shadowRadius": 0,
+          }
+        }
+      >
+        <View
+          accessibilityRole="button"
+          accessible={true}
+          isTVSelectable={true}
+          onResponderGrant={[Function]}
+          onResponderMove={[Function]}
+          onResponderRelease={[Function]}
+          onResponderTerminate={[Function]}
+          onResponderTerminationRequest={[Function]}
+          onStartShouldSetResponder={[Function]}
+          style={
+            Array [
+              Object {
+                "overflow": "hidden",
+              },
+              Array [
+                Object {
+                  "flex": 1,
+                  "width": "100%",
+                },
+                Object {
+                  "borderRadius": 4,
+                },
+              ],
+            ]
+          }
+        >
+          <View
+            style={
+              Array [
+                Object {
+                  "alignItems": "center",
+                  "flexDirection": "row",
+                  "justifyContent": "center",
+                },
+                undefined,
+              ]
+            }
+          >
+            <Text
+              numberOfLines={1}
+              style={
+                Array [
+                  Object {
+                    "color": "#000000",
+                    "fontFamily": "Helvetica Neue",
+                    "textAlign": "left",
+                    "writingDirection": "ltr",
+                  },
+                  Array [
+                    Object {
+                      "letterSpacing": 1,
+                      "marginHorizontal": 16,
+                      "marginVertical": 9,
+                      "textAlign": "center",
+                    },
+                    Object {
+                      "marginHorizontal": 8,
+                    },
+                    Object {
+                      "color": "#6200ee",
+                      "fontFamily": "HelveticaNeue-Medium",
+                    },
+                    Object {
+                      "fontFamily": "HelveticaNeue-Medium",
+                    },
+                  ],
+                ]
+              }
+            >
+              FIRST
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View
+        style={
+          Object {
+            "backgroundColor": "transparent",
+            "borderColor": "transparent",
+            "borderRadius": 4,
+            "borderStyle": "solid",
+            "borderWidth": 0,
+            "elevation": 0,
+            "margin": 4,
+            "minWidth": "auto",
+            "shadowColor": "#000000",
+            "shadowOffset": Object {
+              "height": 0,
+              "width": 0,
+            },
+            "shadowOpacity": 0.24,
+            "shadowRadius": 0,
+          }
+        }
+      >
+        <View
+          accessibilityRole="button"
+          accessible={true}
+          isTVSelectable={true}
+          onResponderGrant={[Function]}
+          onResponderMove={[Function]}
+          onResponderRelease={[Function]}
+          onResponderTerminate={[Function]}
+          onResponderTerminationRequest={[Function]}
+          onStartShouldSetResponder={[Function]}
+          style={
+            Array [
+              Object {
+                "overflow": "hidden",
+              },
+              Array [
+                Object {
+                  "flex": 1,
+                  "width": "100%",
+                },
+                Object {
+                  "borderRadius": 4,
+                },
+              ],
+            ]
+          }
+        >
+          <View
+            style={
+              Array [
+                Object {
+                  "alignItems": "center",
+                  "flexDirection": "row",
+                  "justifyContent": "center",
+                },
+                undefined,
+              ]
+            }
+          >
+            <Text
+              numberOfLines={1}
+              style={
+                Array [
+                  Object {
+                    "color": "#000000",
+                    "fontFamily": "Helvetica Neue",
+                    "textAlign": "left",
+                    "writingDirection": "ltr",
+                  },
+                  Array [
+                    Object {
+                      "letterSpacing": 1,
+                      "marginHorizontal": 16,
+                      "marginVertical": 9,
+                      "textAlign": "center",
+                    },
+                    Object {
+                      "marginHorizontal": 8,
+                    },
+                    Object {
+                      "color": "#6200ee",
+                      "fontFamily": "HelveticaNeue-Medium",
+                    },
+                    Object {
+                      "fontFamily": "HelveticaNeue-Medium",
+                    },
+                  ],
+                ]
+              }
+            >
+              SECOND
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  </View>
+</View>
+`;
+
+exports[`renders visible banner, without action buttons and with image 1`] = `
+<View
+  style={
+    Object {
+      "backgroundColor": "#ffffff",
+      "elevation": 1,
+      "shadowColor": "#000000",
+      "shadowOffset": Object {
+        "height": 0.5,
+        "width": 0,
+      },
+      "shadowOpacity": 0.24,
+      "shadowRadius": 0.75,
+    }
+  }
+>
+  <View
+    style={
+      Object {
+        "height": 0,
+      }
+    }
+  />
+  <View
+    onLayout={[Function]}
+    style={Object {}}
+  >
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-start",
+          "marginBottom": 0,
+          "marginHorizontal": 8,
+          "marginTop": 16,
+        }
+      }
+    >
+      <View
+        style={
+          Object {
+            "margin": 8,
+          }
+        }
+      >
+        <Image
+          source={
+            Object {
+              "uri": "https://callstack.com/images/team/Satya.png",
+            }
+          }
+          style={
+            Object {
+              "height": 40,
+              "width": 40,
+            }
+          }
+        />
+      </View>
+      <Text
+        style={
+          Array [
+            Object {
+              "color": "#000000",
+              "fontFamily": "Helvetica Neue",
+              "textAlign": "left",
+              "writingDirection": "ltr",
+            },
+            Object {
+              "flex": 1,
+              "margin": 8,
+            },
+          ]
+        }
+      >
+        Two line text string with two actions. One to two lines is preferable on mobile.
+      </Text>
+    </View>
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-end",
+          "margin": 4,
+        }
+      }
+    />
+  </View>
+</View>
+`;
+
+exports[`renders visible banner, without action buttons and without image 1`] = `
+<View
+  style={
+    Object {
+      "backgroundColor": "#ffffff",
+      "elevation": 1,
+      "shadowColor": "#000000",
+      "shadowOffset": Object {
+        "height": 0.5,
+        "width": 0,
+      },
+      "shadowOpacity": 0.24,
+      "shadowRadius": 0.75,
+    }
+  }
+>
+  <View
+    style={
+      Object {
+        "height": 0,
+      }
+    }
+  />
+  <View
+    onLayout={[Function]}
+    style={Object {}}
+  >
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-start",
+          "marginBottom": 0,
+          "marginHorizontal": 8,
+          "marginTop": 16,
+        }
+      }
+    >
+      <Text
+        style={
+          Array [
+            Object {
+              "color": "#000000",
+              "fontFamily": "Helvetica Neue",
+              "textAlign": "left",
+              "writingDirection": "ltr",
+            },
+            Object {
+              "flex": 1,
+              "margin": 8,
+            },
+          ]
+        }
+      >
+        Two line text string with two actions. One to two lines is preferable on mobile.
+      </Text>
+    </View>
+    <View
+      style={
+        Object {
+          "flexDirection": "row",
+          "justifyContent": "flex-end",
+          "margin": 4,
+        }
+      }
+    />
+  </View>
+</View>
+`;
