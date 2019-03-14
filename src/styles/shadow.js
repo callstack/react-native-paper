820480454ecf592f419056e2108f9041afa06506
Julian Hundeloh <julian@hundeloh-consulting.ch>
2019-03-14 14:16:21 +0100

    fix: use animated values

--- src/styles/shadow.js
+++ src/styles/shadow.js
@@ -3,47 +3,54 @@
 import * as Colors from './colors';
 import { Animated } from 'react-native';
 
+function getRadius(elevation: ?number) {
+  const mapping = {
+    1: 0.75,
+    2: 1.5,
+  };
+
+  return mapping[elevation] || elevation;
+}
+
 export default function shadow(elevation: number | Animated.Value) {
-  let height, radius, width;
+  const animatedElevation: Animated.Value =
+    elevation instanceof Animated.Value
+      ? elevation
+      : new Animated.Value(elevation || 0);
+  const inputRange = [0, 1, 2, 3, 8, 24];
 
-  if (elevation instanceof Animated.Value) {
-    width = elevation.interpolate({
-      inputRange: [0, 1, 2, 3, 8, 24],
-      outputRange: [0, 0, 0, 0, 0, 0],
-    });
+  // We need to return Animated properties to ensure they are properly used in the Animated.View
+  const shadowOpacity = new Animated.Value(0.24);
 
-    height = elevation.interpolate({
-      inputRange: [0, 1, 2, 3, 8, 24],
-      outputRange: [0, 0, 0, 0, 0, 0],
-    });
+  // We cannot used strings in Animated.Value and hence we need to interpolate something
+  const shadowColor = animatedElevation.interpolate({
+    inputRange,
+    outputRange: inputRange.map(() => Colors.black),
+  });
 
-    radius = elevation.interpolate({
-      inputRange: [0, 1, 2, 3, 8, 24],
-      outputRange: [0, 0.75, 1.5, 3, 8, 24],
-    });
-  } else {
-    width = 0;
-    height = 0;
+  // Width and height are required to be numbers on web (i.e. not undefined)
+  const width = animatedElevation.interpolate({
+    inputRange,
+    outputRange: [0, 0, 0, 0, 0, 0],
+  });
+  const height = animatedElevation.interpolate({
+    inputRange,
+    outputRange: [0, 0, 0, 0, 0, 0],
+  });
 
-    switch (elevation) {
-      case 1:
-        radius = 0.75;
-        break;
-      case 2:
-        radius = 1.5;
-        break;
-      default:
-        radius = elevation;
-    }
-  }
+  // Calculate radius based on elevation
+  const shadowRadius = animatedElevation.interpolate({
+    inputRange,
+    outputRange: inputRange.map(getRadius),
+  });
 
   return {
-    shadowColor: Colors.black,
+    shadowColor,
     shadowOffset: {
       width,
       height,
     },
-    shadowOpacity: 0.24,
-    shadowRadius: radius,
+    shadowOpacity,
+    shadowRadius,
   };
 }
