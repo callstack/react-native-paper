8e0ac996d2416d97563afd802abe85aec09728df
Jordy Wijman <jordywijman@gmail.com>
2019-04-24 15:36:40 +0200

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -221,8 +221,11 @@ class Button extends React.Component<Props, State> {
     };
     const touchableStyle = { borderRadius: roundness };
     const textStyle = { color: textColor, fontFamily };
-    const elevation =
-      disabled || mode !== 'contained' ? 0 : this.state.elevation;
+    const elevation = disabled
+      ? 0
+      : mode !== 'contained'
+        ? new Animated.Value(0)
+        : this.state.elevation;
 
     return (
       <Surface
