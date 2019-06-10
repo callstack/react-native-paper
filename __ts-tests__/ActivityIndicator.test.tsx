a4fae9f3beffa2d6c4d81ee618acdadc770c2605
raajnadar <raajnadar@gmail.com>
2019-06-10 19:35:06 +0530

    fix: snapshots and remove ts-tests

--- __ts-tests__/ActivityIndicator.test.tsx
+++
@@ -1,9 +0,0 @@
-
-import * as React from 'react';
-import { ActivityIndicator, Colors } from '..';
-
-const MyComponent = () => (
-  <ActivityIndicator animating={true} color={Colors.red800} />
-);
-
-export default MyComponent;
--- __ts-tests__/AvatarIcon.test.tsx
+++
@@ -1,7 +0,0 @@
-
-import * as React from 'react';
-import { Avatar } from '..';
-
-const MyComponent = () => (
-  <Avatar.Icon size={24} icon="folder" />
-);
