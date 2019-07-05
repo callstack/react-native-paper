dddc5f00d0b69dd40b5753bd4e0225b11787e218
jaulz <jaulz@users.noreply.github.com>
2019-07-05 14:16:29 +0200

    fix: reduce delay of Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -262,7 +262,7 @@ class Menu extends React.Component<Props, State> {
       !anchorLayout.width ||
       !anchorLayout.height
     ) {
-      setTimeout(this._show, ANIMATION_DURATION);
+      setImmediate(this._show);
       return;
     }
 
