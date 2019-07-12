095174f1052f3d8ab770a5624c2d44955a1e70e3
Julian Hundeloh <jaulz@users.noreply.github.com>
2019-07-12 09:01:05 +0200

    fix: run prettier

--- src/core/theming.tsx
+++ src/core/theming.tsx
@@ -2,4 +2,6 @@ import { createTheming } from '@callstack/react-theme-provider';
 import DefaultTheme from '../styles/DefaultTheme';
 import { Theme } from '../types';
 
-export const { ThemeProvider, withTheme, useTheme } = createTheming<Theme>(DefaultTheme);
+export const { ThemeProvider, withTheme, useTheme } = createTheming<Theme>(
+  DefaultTheme
+);
