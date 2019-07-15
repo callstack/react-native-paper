c9a5095e5e60fe495667e444977503d68c0e3de4
lukewalczak <lukasz.walczak.pwr@gmail.com>
2019-07-15 11:24:08 +0200

    feat: extend chip with onLongPress functionality

--- example/src/Examples/ChipExample.tsx
+++ example/src/Examples/ChipExample.tsx
@@ -1,206 +1,238 @@
 import * as React from 'react';
 import { View, ScrollView, StyleSheet, Image } from 'react-native';
-import { Chip, List, withTheme, Theme } from 'react-native-paper';
+import { Chip, List, withTheme, Theme, Snackbar } from 'react-native-paper';
 import color from 'color';
 
 type Props = {
   theme: Theme;
 };
 
-class ChipExample extends React.Component<Props> {
+type State = {
+  visible: boolean;
+};
+
+class ChipExample extends React.Component<Props, State> {
   static title = 'Chip';
+
+  state = {
+    visible: false,
+  };
 
   render() {
     const { colors } = this.props.theme;
 
     return (
-      <ScrollView
-        style={[styles.container, { backgroundColor: colors.surface }]}
-      >
-        <List.Section title="Flat chip">
-          <View style={styles.row}>
-            <Chip
-              mode="outlined"
-              selected
-              onPress={() => {}}
-              style={styles.chip}
-            >
-              Simple
-            </Chip>
-            <Chip onPress={() => {}} onClose={() => {}} style={styles.chip}>
-              Close button
-            </Chip>
-            <Chip
-              icon="favorite"
-              onPress={() => {}}
-              onClose={() => {}}
-              style={styles.chip}
-            >
-              Icon
-            </Chip>
-            <Chip
-              avatar={
-                <Image source={require('../../assets/images/avatar.png')} />
-              }
-              onPress={() => {}}
-              onClose={() => {}}
-              style={styles.chip}
-            >
-              Avatar
-            </Chip>
-            <Chip
-              selected
-              avatar={
-                <Image source={require('../../assets/images/avatar.png')} />
-              }
-              onPress={() => {}}
-              style={styles.chip}
-            >
-              Avatar (selected)
-            </Chip>
-            <Chip
-              disabled
-              icon="favorite"
-              onClose={() => {}}
-              style={styles.chip}
-            >
-              Icon (disabled)
-            </Chip>
-            <Chip
-              disabled
-              avatar={
-                <Image source={require('../../assets/images/avatar.png')} />
-              }
-              style={styles.chip}
-            >
-              Avatar (disabled)
-            </Chip>
-          </View>
-        </List.Section>
-        <List.Section title="Outlined chip">
-          <View style={styles.row}>
-            <Chip mode="outlined" onPress={() => {}} style={styles.chip}>
-              Simple
-            </Chip>
-            <Chip
-              mode="outlined"
-              onPress={() => {}}
-              onClose={() => {}}
-              style={styles.chip}
-            >
-              Close button
-            </Chip>
-            <Chip
-              mode="outlined"
-              icon="favorite"
-              onPress={() => {}}
-              onClose={() => {}}
-              style={styles.chip}
-            >
-              Icon
-            </Chip>
-            <Chip
-              mode="outlined"
-              avatar={
-                <Image source={require('../../assets/images/avatar.png')} />
-              }
-              onPress={() => {}}
-              style={styles.chip}
-            >
-              Avatar
-            </Chip>
-            <Chip
-              selected
-              mode="outlined"
-              avatar={
-                <Image source={require('../../assets/images/avatar.png')} />
-              }
-              onPress={() => {}}
-              style={styles.chip}
-            >
-              Avatar (selected)
-            </Chip>
-            <Chip
-              disabled
-              mode="outlined"
-              icon="favorite"
-              onClose={() => {}}
-              style={styles.chip}
-            >
-              Icon (disabled)
-            </Chip>
-            <Chip
-              disabled
-              mode="outlined"
-              avatar={
-                <Image source={require('../../assets/images/avatar.png')} />
-              }
-              style={styles.chip}
-            >
-              Avatar (disabled)
-            </Chip>
-          </View>
-        </List.Section>
-        <List.Section title="Custom chip">
-          <View style={styles.row}>
-            <Chip
-              selected
-              onPress={() => {}}
-              style={[
-                styles.chip,
-                {
-                  backgroundColor: color(colors.primary)
-                    .alpha(0.2)
-                    .rgb()
-                    .string(),
-                },
-              ]}
-              selectedColor={colors.primary}
-            >
-              Flat selected chip with custom color
-            </Chip>
-            <Chip
-              onPress={() => {}}
-              style={styles.chip}
-              selectedColor={colors.primary}
-            >
-              Flat unselected chip with custom color
-            </Chip>
-            <Chip
-              selected
-              mode="outlined"
-              onPress={() => {}}
-              style={[
-                styles.chip,
-                {
-                  backgroundColor: color(colors.primary)
-                    .alpha(0.2)
-                    .rgb()
-                    .string(),
-                },
-              ]}
-              selectedColor={colors.primary}
-            >
-              Outlined selected chip with custom color
-            </Chip>
-            <Chip
-              mode="outlined"
-              onPress={() => {}}
-              style={styles.chip}
-              selectedColor={colors.primary}
-            >
-              Outlined unselected chip with custom color
-            </Chip>
-            <Chip
-              onPress={() => {}}
-              style={styles.chip}
-              textStyle={styles.tiny}
-            >
-              With custom size
-            </Chip>
-          </View>
-        </List.Section>
-      </ScrollView>
+      <>
+        <ScrollView
+          style={[styles.container, { backgroundColor: colors.surface }]}
+        >
+          <List.Section title="Flat chip">
+            <View style={styles.row}>
+              <Chip
+                mode="outlined"
+                selected
+                onPress={() => {}}
+                style={styles.chip}
+              >
+                Simple
+              </Chip>
+              <Chip onPress={() => {}} onClose={() => {}} style={styles.chip}>
+                Close button
+              </Chip>
+              <Chip
+                icon="favorite"
+                onPress={() => {}}
+                onClose={() => {}}
+                style={styles.chip}
+              >
+                Icon
+              </Chip>
+              <Chip
+                avatar={
+                  <Image source={require('../../assets/images/avatar.png')} />
+                }
+                onPress={() => {}}
+                onClose={() => {}}
+                style={styles.chip}
+              >
+                Avatar
+              </Chip>
+              <Chip
+                selected
+                avatar={
+                  <Image source={require('../../assets/images/avatar.png')} />
+                }
+                onPress={() => {}}
+                style={styles.chip}
+              >
+                Avatar (selected)
+              </Chip>
+              <Chip
+                disabled
+                icon="favorite"
+                onClose={() => {}}
+                style={styles.chip}
+              >
+                Icon (disabled)
+              </Chip>
+              <Chip
+                disabled
+                avatar={
+                  <Image source={require('../../assets/images/avatar.png')} />
+                }
+                style={styles.chip}
+              >
+                Avatar (disabled)
+              </Chip>
+            </View>
+          </List.Section>
+          <List.Section title="Outlined chip">
+            <View style={styles.row}>
+              <Chip mode="outlined" onPress={() => {}} style={styles.chip}>
+                Simple
+              </Chip>
+              <Chip
+                mode="outlined"
+                onPress={() => {}}
+                onClose={() => {}}
+                style={styles.chip}
+              >
+                Close button
+              </Chip>
+              <Chip
+                mode="outlined"
+                icon="favorite"
+                onPress={() => {}}
+                onClose={() => {}}
+                style={styles.chip}
+              >
+                Icon
+              </Chip>
+              <Chip
+                mode="outlined"
+                avatar={
+                  <Image source={require('../../assets/images/avatar.png')} />
+                }
+                onPress={() => {}}
+                style={styles.chip}
+              >
+                Avatar
+              </Chip>
+              <Chip
+                selected
+                mode="outlined"
+                avatar={
+                  <Image source={require('../../assets/images/avatar.png')} />
+                }
+                onPress={() => {}}
+                style={styles.chip}
+              >
+                Avatar (selected)
+              </Chip>
+              <Chip
+                disabled
+                mode="outlined"
+                icon="favorite"
+                onClose={() => {}}
+                style={styles.chip}
+              >
+                Icon (disabled)
+              </Chip>
+              <Chip
+                disabled
+                mode="outlined"
+                avatar={
+                  <Image source={require('../../assets/images/avatar.png')} />
+                }
+                style={styles.chip}
+              >
+                Avatar (disabled)
+              </Chip>
+            </View>
+          </List.Section>
+          <List.Section title="Custom chip">
+            <View style={styles.row}>
+              <Chip
+                mode="outlined"
+                onPress={() => {}}
+                onLongPress={() => {
+                  this.setState({ visible: true });
+                }}
+                style={styles.chip}
+              >
+                With onLongPress
+              </Chip>
+
+              <Chip
+                selected
+                onPress={() => {}}
+                style={[
+                  styles.chip,
+                  {
+                    backgroundColor: color(colors.primary)
+                      .alpha(0.2)
+                      .rgb()
+                      .string(),
+                  },
+                ]}
+                selectedColor={colors.primary}
+              >
+                Flat selected chip with custom color
+              </Chip>
+              <Chip
+                onPress={() => {}}
+                style={styles.chip}
+                selectedColor={colors.primary}
+              >
+                Flat unselected chip with custom color
+              </Chip>
+              <Chip
+                selected
+                mode="outlined"
+                onPress={() => {}}
+                style={[
+                  styles.chip,
+                  {
+                    backgroundColor: color(colors.primary)
+                      .alpha(0.2)
+                      .rgb()
+                      .string(),
+                  },
+                ]}
+                selectedColor={colors.primary}
+              >
+                Outlined selected chip with custom color
+              </Chip>
+              <Chip
+                mode="outlined"
+                onPress={() => {}}
+                style={styles.chip}
+                selectedColor={colors.primary}
+              >
+                Outlined unselected chip with custom color
+              </Chip>
+              <Chip
+                onPress={() => {}}
+                style={styles.chip}
+                textStyle={styles.tiny}
+              >
+                With custom size
+              </Chip>
+            </View>
+          </List.Section>
+        </ScrollView>
+        <Snackbar
+          visible={this.state.visible}
+          onDismiss={() => this.setState({ visible: false })}
+          action={{
+            label: 'Undo',
+            onPress: () => {},
+          }}
+          duration={Snackbar.DURATION_SHORT}
+        >
+          onLongPress activated!
+        </Snackbar>
+      </>
     );
   }
 }
--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -59,6 +59,10 @@ type Props = React.ComponentProps<typeof Surface> & {
    */
   onPress?: () => void;
   /**
+   * Function to execute on long press.
+   */
+  onLongPress?: () => void;
+  /**
    * Function to execute on close button press. The close button appears only when this prop is specified.
    */
   onClose?: () => void;
@@ -143,6 +147,7 @@ class Chip extends React.Component<Props, State> {
       disabled,
       accessibilityLabel,
       onPress,
+      onLongPress,
       onClose,
       textStyle,
       style,
@@ -235,6 +240,7 @@ class Chip extends React.Component<Props, State> {
           delayPressIn={0}
           style={{ borderRadius }}
           onPress={onPress}
+          onLongPress={onLongPress}
           onPressIn={this._handlePressIn}
           onPressOut={this._handlePressOut}
           underlayColor={underlayColor}
