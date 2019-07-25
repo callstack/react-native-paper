2e643bf4ccf0fcdd58331bfb0a60056e51024fff
jbinda <jakub.binda@gmail.com>
2019-07-25 10:21:22 +0200

    refactor: adjust label margin on minimized state

--- src/components/TextInput/Label/LabelBackground.tsx
+++ src/components/TextInput/Label/LabelBackground.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Animated, StyleSheet } from 'react-native';
 
 import AnimatedText from '../../Typography/AnimatedText';
 
@@ -9,52 +9,91 @@ const LabelBackground = ({
   parentState,
   labelProps: {
     placeholderStyle,
+    baseLabelTranslateX,
     topPosition,
     hasActiveOutline,
     label,
     backgroundColor,
   },
   labelStyle,
-}: LabelBackgroundProps) =>
-  label ? (
-    <AnimatedText
-      style={[
-        placeholderStyle,
-        labelStyle,
-        {
-          top: topPosition + 1,
-          transform: [
-            ...labelStyle.transform,
+}: LabelBackgroundProps) => {
+  const hasFocus = hasActiveOutline || parentState.value;
+  const opacity = parentState.labeled.interpolate({
+    inputRange: [0, 1],
+    outputRange: [hasFocus ? 1 : 0, 0],
+  });
+
+  const labelTranslationX = {
+    transform: [
+      {
+        translateX: parentState.labeled.interpolate({
+          inputRange: [0, 1],
+          outputRange: [-baseLabelTranslateX, 0],
+        }),
+      },
+    ],
+  };
+
+  return label
+    ? [
+        <Animated.View
+          key="labelBackground-view"
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            styles.view,
             {
-              scaleY: parentState.labeled.interpolate({
-                inputRange: [0, 1],
-                outputRange: [0.2, 1],
-              }),
+              backgroundColor,
+              opacity,
             },
-          ],
-        },
-        styles.outlinedLabel || {},
-        {
-          backgroundColor,
-          opacity: parentState.labeled.interpolate({
-            inputRange: [0, 1],
-            outputRange: [hasActiveOutline || parentState.value ? 1 : 0, 0],
-          }),
-        },
-      ]}
-      numberOfLines={1}
-    >
-      {label}
-    </AnimatedText>
-  ) : null;
+            labelTranslationX,
+          ]}
+        />,
+        <AnimatedText
+          key="labelBackground-text"
+          style={[
+            placeholderStyle,
+            labelStyle,
+            {
+              top: topPosition + 1,
+              transform: [
+                ...labelStyle.transform,
+                {
+                  scaleY: parentState.labeled.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0.2, 1],
+                  }),
+                },
+              ],
+            },
+            styles.outlinedLabel || {},
+            {
+              backgroundColor,
+              opacity,
+            },
+          ]}
+          numberOfLines={1}
+        >
+          {label}
+        </AnimatedText>,
+      ]
+    : null;
+};
 
 export default LabelBackground;
 
 const styles = StyleSheet.create({
+  view: {
+    position: 'absolute',
+    top: 6,
+    left: 10,
+    width: 8,
+    height: 2,
+  },
   outlinedLabel: {
     position: 'absolute',
-    left: 10,
-    paddingHorizontal: 4,
+    left: 18,
+    paddingHorizontal: 0,
     color: 'transparent',
   },
 });
