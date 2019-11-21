172a65854bb9d8d96305ec5c12a16977c13fc65a
Bruno <bruno.castro@codeminer42.com>
2019-11-21 15:40:27 -0300

    refactor: HOC WithRadioContext

--- src/components/RadioButton/WithRadioContext.tsx
+++ src/components/RadioButton/WithRadioContext.tsx
@@ -1,31 +1,39 @@
-import React, { useContext } from 'react';
-import { RadioButtonContext } from './RadioButtonGroup';
+import React from 'react';
+import { RadioButtonContext, RadioButtonContextType } from './RadioButtonGroup';
 import { Props } from './RadioButton';
 
 const withRadioContext = (
   RadioComponent: React.ComponentType<Omit<Props, 'theme'>>
 ) => {
   return (props: Omit<Props, 'theme'>) => {
-    const context = useContext(RadioButtonContext);
-
-    const handlePress = () => {
-      const { onPress, value } = props;
-
-      context ? context.onValueChange(value) : onPress && onPress();
-    };
-
-    const isChecked = () => {
-      const { value, status } = props;
-
-      if (context) {
-        return context.value === value ? 'checked' : 'unchecked';
-      } else {
-        return status;
-      }
-    };
-
     return (
-      <RadioComponent {...props} status={isChecked()} onPress={handlePress} />
+      <RadioButtonContext.Consumer>
+        {(context: RadioButtonContextType) => {
+          const handlePress = () => {
+            const { onPress, value } = props;
+
+            context ? context.onValueChange(value) : onPress && onPress();
+          };
+
+          const isChecked = () => {
+            const { value, status } = props;
+
+            if (context) {
+              return context.value === value ? 'checked' : 'unchecked';
+            } else {
+              return status;
+            }
+          };
+
+          return (
+            <RadioComponent
+              {...props}
+              status={isChecked()}
+              onPress={handlePress}
+            />
+          );
+        }}
+      </RadioButtonContext.Consumer>
     );
   };
 };
