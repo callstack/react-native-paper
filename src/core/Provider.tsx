b9a187d923a3da376a404e94786b89a674cd6157
Mariia Marchenkova <marchewka@Mariias-MacBook-Pro.local>
2020-01-28 11:45:47 +0100

    feat: check if refuce motion enabled, update animation scale

--- src/core/Provider.tsx
+++ src/core/Provider.tsx
@@ -1,4 +1,6 @@
+// @ts-nocheck
 import * as React from 'react';
+import { AccessibilityInfo } from 'react-native';
 import { ThemeProvider } from './theming';
 import { Provider as SettingsProvider, Settings } from './settings';
 import MaterialCommunityIcon from '../components/MaterialCommunityIcon';
@@ -10,8 +12,37 @@ type Props = {
   theme?: Theme;
   settings?: Settings;
 };
-
 export default class Provider extends React.Component<Props> {
+  state = {
+    reduceMotionEnabled: false,
+  };
+
+  componentDidMount() {
+    AccessibilityInfo.addEventListener(
+      'reduceMotionEnabled',
+      this._handleReduceMotionToggled
+    );
+
+    AccessibilityInfo.isReduceMotionEnabled()
+      .then((reduceMotionEnabled: boolean) => {
+        const { scale } = this.props.settings;
+        this.setState({ reduceMotionEnabled });
+        return (scale.value = 0);
+      })
+      .catch(e => console.log(e));
+  }
+
+  componentWillUnmount() {
+    AccessibilityInfo.removeEventListener(
+      'reduceMotionEnabled',
+      this._handleReduceMotionToggled
+    );
+  }
+
+  _handleReduceMotionToggled = (reduceMotionEnabled: any) => {
+    this.setState({ reduceMotionEnabled });
+  };
+
   render() {
     return (
       <PortalHost>
