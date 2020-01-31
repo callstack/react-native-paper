3a17fd1b8606991b5bd66b5e18270168943188f4
Trancever <dawidu@onet.pl>
2020-01-31 15:42:27 +0100

    feat: a small test

--- example/src/ExampleList.tsx
+++ example/src/ExampleList.tsx
@@ -87,6 +87,7 @@ const data = Object.keys(examples).map(
 export default function ExampleList({ navigation }: Props) {
   const renderItem = ({ item }: { item: Item }) => (
     <List.Item
+      style={{ backgroundColor: '#BADA' }}
       title={item.data.title}
       onPress={() => navigation.navigate(item.id)}
     />
