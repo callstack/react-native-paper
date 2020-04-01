e3ed028dce7f66190a6622e7471029dbdd098b85
Mauro Guardarini <mauro.guardarini@lambda3.com.br>
2020-04-01 00:14:10 -0300

    fix: added testID to TextInput component

--- src/components/__tests__/TextInput.test.js
+++ src/components/__tests__/TextInput.test.js
@@ -3,12 +3,12 @@ import { render } from 'react-native-testing-library';
 import TextInput from '../TextInput/TextInput';
 
 it('renders on TextInput', () => {
- const { toJSON } = render(<TextInput testID="input" />);
- expect( toJSON() ).toMatchSnapshot();
+  const { toJSON } = render(<TextInput testID="input" />);
+  expect(toJSON()).toMatchSnapshot();
 });
 
 it('should be find TextInput component by testID', () => {
- const { getByTestId } = render(<TextInput testID="input" />);
- const component = getByTestId('input');
- expect(component).toBeTruthy();
+  const { getByTestId } = render(<TextInput testID="input" />);
+  const component = getByTestId('input');
+  expect(component).toBeTruthy();
 });
