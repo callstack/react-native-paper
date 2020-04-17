88431c57cfb4c61ae51e2400930224f55449cfe7
joshglenn <joshglenn@users.noreply.github.com>
2020-04-17 04:53:41 -0500

    docs: fix the FAB.Group example in documentation (#1819)

--- src/components/FAB/FABGroup.tsx
+++ src/components/FAB/FABGroup.tsx
@@ -118,9 +118,9 @@ type State = {
  *          <Portal>
  *            <FAB.Group
  *              open={open}
- *              icon={open ? 'today' : 'add'}
+ *              icon={open ? 'calendar-today' : 'plus'}
  *              actions={[
- *                { icon: 'add', onPress: () => console.log('Pressed add') },
+ *                { icon: 'plus', onPress: () => console.log('Pressed add') },
  *                { icon: 'star', label: 'Star', onPress: () => console.log('Pressed star')},
  *                { icon: 'email', label: 'Email', onPress: () => console.log('Pressed email') },
  *                { icon: 'bell', label: 'Remind', onPress: () => console.log('Pressed notifications') },
