2900b04190e3ca8ec2ecc14a00c7628fead6d81b
Stheffany Santos Hadlich <stheffanyhadlich@gmail.com>
2020-04-24 04:37:20 -0300

    feat: create Checkbox.Item component

--- src/components/Checkbox/CheckboxIOS.tsx
+++ src/components/Checkbox/CheckboxIOS.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { StyleSheet, View, I18nManager } from 'react-native';
 import color from 'color';
-import MaterialCommunityIcon from './MaterialCommunityIcon';
-import TouchableRipple from './TouchableRipple';
-import { withTheme } from '../core/theming';
-import { Theme, $RemoveChildren } from '../types';
+import MaterialCommunityIcon from '../MaterialCommunityIcon';
+import TouchableRipple from '../TouchableRipple';
+import { withTheme } from '../../core/theming';
+import { Theme, $RemoveChildren } from '../../types';
 
 type Props = $RemoveChildren<typeof TouchableRipple> & {
   /**
