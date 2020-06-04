97cb908d20e921a73d56493a37f47f8230d07904
Mariia Marchenkova <marchewka@Mariias-MacBook-Pro.local>
2020-06-04 09:55:47 +0200

    docs: changed favicon

--- docs/component-docs.config.js
+++ docs/component-docs.config.js
@@ -126,7 +126,7 @@ function getPages() {
 module.exports = {
   root,
   logo: 'images/sidebar-logo.svg',
-  favicon: 'images/paper.ico',
+  favicon: 'images/favicon.ico',
   assets,
   styles,
   scripts,
