87fb26e348c5105c883722f1f97faa407970f607
Dawid Urbaniak <dawidu@onet.pl>
2020-07-16 10:00:09 +0200

    chore: bump rn web and add async storage that supports web (example app) (#2057)

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { AsyncStorage, I18nManager, Platform, YellowBox } from 'react-native';
+import { I18nManager, Platform, YellowBox } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { Updates } from 'expo';
 import { useKeepAwake } from 'expo-keep-awake';
 import { InitialState, NavigationContainer } from '@react-navigation/native';
