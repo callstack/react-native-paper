callstack/react-native-paper

commit ef98d6b668113ca33384729867032c5d455ff7ab

fix: pass useNativeDriver to all animations (#1787)

src/components/Card/Card.tsx
@@ -110,7 +110,7 @@ class Card extends React.Component<Props, State> {
     Animated.timing(this.state.elevation, {
       toValue: 8,
       duration: 150 * scale,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   };
 
@@ -120,7 +120,7 @@ class Card extends React.Component<Props, State> {
       // @ts-ignore
       toValue: this.props.elevation,
       duration: 150 * scale,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   };
 

src/components/CheckboxAndroid.tsx
@@ -74,12 +74,14 @@ class CheckboxAndroid extends React.Component<Props, State> {
       Animated.timing(this.state.scaleAnim, {
         toValue: 0.85,
         duration: checked ? ANIMATION_DURATION * animation.scale : 0,
+        useNativeDriver: false,
       }),
       Animated.timing(this.state.scaleAnim, {
         toValue: 1,
         duration: checked
           ? ANIMATION_DURATION * animation.scale
           : ANIMATION_DURATION * animation.scale * 1.75,
+        useNativeDriver: false,
       }),
     ]).start();
   }

src/components/Chip.tsx
@@ -128,7 +128,7 @@ class Chip extends React.Component<Props, State> {
     Animated.timing(this.state.elevation, {
       toValue: 4,
       duration: 200 * scale,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   };
 
@@ -137,7 +137,7 @@ class Chip extends React.Component<Props, State> {
     Animated.timing(this.state.elevation, {
       toValue: 0,
       duration: 150 * scale,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   };
 

src/components/CrossFadeIcon.tsx
@@ -68,7 +68,7 @@ class CrossFadeIcon extends React.Component<Props, State> {
     Animated.timing(this.state.fade, {
       duration: scale * 200,
       toValue: 0,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   }
 

commit d2eef010218817aeecf62f102cdc86e031a4c561

fix: ensure compatible animation driver for cards
On dark themes, the default mode is adaptive. This mode will change the `backgroundColor`. This property is not animatable using the nativeDriver. Upstream issue: https://github.com/facebook/react-native/issues/14178

The bug was introduced in https://github.com/callstack/react-native-paper/pull/1787
Fixes https://github.com/callstack/react-native-paper/issues/2063

src/components/Card/Card.tsx
@@ -105,21 +105,29 @@ class Card extends React.Component<Props, State> {
   };
 
   private handlePressIn = () => {
-    const { scale } = this.props.theme.animation;
+    const {
+      dark,
+      mode,
+      animation: { scale },
+    } = this.props.theme;
     Animated.timing(this.state.elevation, {
       toValue: 8,
       duration: 150 * scale,
-      useNativeDriver: true,
+      useNativeDriver: !dark || mode === 'exact',
     }).start();
   };
 
   private handlePressOut = () => {
-    const { scale } = this.props.theme.animation;
+    const {
+      dark,
+      mode,
+      animation: { scale },
+    } = this.props.theme;
     Animated.timing(this.state.elevation, {
       // @ts-ignore
       toValue: this.props.elevation,
       duration: 150 * scale,
-      useNativeDriver: true,
+      useNativeDriver: !dark || mode === 'exact',
     }).start();
   };
 