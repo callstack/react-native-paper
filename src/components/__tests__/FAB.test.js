7012652ad793df02757da0a6dfe03e2192a094f0
Leonardo Nogueira <leonardo.nogueira@dextra-sw.com>
2020-10-26 13:07:40 -0300

    test: added test to FAB component

--- src/components/__tests__/FAB.test.js
+++ src/components/__tests__/FAB.test.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import renderer from 'react-test-renderer';
 import FAB from '../FAB/FAB.tsx';
 
+jest.useFakeTimers();
+
 it('renders normal FAB', () => {
   const tree = renderer.create(<FAB onPress={() => {}} icon="plus" />).toJSON();
 
@@ -22,4 +24,47 @@ it('renders extended FAB', () => {
     .toJSON();
 
   expect(tree).toMatchSnapshot();
+});
+
+it('renders loading FAB', () => {
+  const tree = renderer
+    .create(<FAB onPress={() => {}} icon="plus" loading={true} />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders disabled FAB', () => {
+  const tree = renderer
+    .create(<FAB onPress={() => {}} icon="plus" disabled />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders custom color for the icon and label of the FAB', () => {
+  const tree = renderer
+    .create(<FAB onPress={() => {}} icon="plus" color="#AA0114" />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders not visible FAB', () => {
+  const { update, toJSON } = renderer.create(
+    <FAB onPress={() => {}} icon="plus" />
+  );
+  update(<FAB onPress={() => {}} icon="plus" visible={false} />);
+
+  expect(toJSON()).toMatchSnapshot();
+});
+
+it('renders visible FAB', () => {
+  const { update, toJSON } = renderer.create(
+    <FAB onPress={() => {}} icon="plus" visible={false} />
+  );
+
+  update(<FAB onPress={() => {}} icon="plus" visible={true} />);
+
+  expect(toJSON()).toMatchSnapshot();
 });
