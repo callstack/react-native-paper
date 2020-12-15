6ffb9aeab66611ac575995d6c7009b877b106b9f
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-12-15 17:58:23 +0100

    chore: fix bootstrap script

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,15 +1,21 @@
+const path = require('path');
 const child_process = require('child_process');
 
+const root = path.resolve(__dirname, '..');
 const args = process.argv.slice(2);
 const options = {
   cwd: process.cwd(),
   env: process.env,
-  stdio: [process.stdin, process.stdout, process.stderr],
+  stdio: 'inherit',
   encoding: 'utf-8',
 };
 
-if (args.length) {
-  child_process.spawnSync('yarn', args, options);
+let result;
+
+if (process.cwd() !== root || args.length) {
+  result = child_process.spawnSync('yarn', args, options);
 } else {
-  child_process.spawnSync('yarn', ['bootstrap'], options);
+  result = child_process.spawnSync('yarn', ['bootstrap'], options);
 }
+
+process.exitCode = result.status;
