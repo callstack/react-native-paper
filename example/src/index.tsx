620a4f2410dbad7e963d6f85c23f918a300108e9
Trancever <dawidu@onet.pl>
2021-01-13 10:19:18 +0100

    feat: check publish action

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -14,6 +14,7 @@ import App from './RootNavigator';
 import DrawerItems from './DrawerItems';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+
 // Add new typescript properties to the theme
 declare global {
   namespace ReactNativePaper {
