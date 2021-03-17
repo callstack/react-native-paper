2471147bebefbf8ae0ee379cfa2767fbe71797ec
shubham jha <shubham@juakali.io>
2021-03-17 08:50:20 +0530

    feat: snapshots updated

--- src/babel/__fixtures__/rewrite-imports/output.js
+++ src/babel/__fixtures__/rewrite-imports/output.js
@@ -1,11 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Text } from 'react-native';
-import PaperProvider from "react-native-paper/lib/module/core/Provider";
-import BottomNavigation from "react-native-paper/lib/module/components/BottomNavigation";
-import Button from "react-native-paper/lib/module/components/Button";
-import FAB from "react-native-paper/lib/module/components/FAB";
-import Appbar from "react-native-paper/lib/module/components/Appbar";
-import * as Colors from "react-native-paper/lib/module/styles/colors";
+import PaperProvider from "react-native-paper/lib\\module\\core\\Provider";
+import BottomNavigation from "react-native-paper/lib\\module\\components\\BottomNavigation";
+import Button from "react-native-paper/lib\\module\\components\\Button";
+import FAB from "react-native-paper/lib\\module\\components\\FAB";
+import Appbar from "react-native-paper/lib\\module\\components\\Appbar";
+import * as Colors from "react-native-paper/lib\\module\\styles\\colors";
 import { NonExistent, NonExistentSecond as Stuff, Theme } from "react-native-paper/lib/module/index.js";
-import { ThemeProvider } from "react-native-paper/lib/module/core/theming";
-import { withTheme } from "react-native-paper/lib/module/core/theming";
+import { ThemeProvider } from "react-native-paper/lib\\module\\core\\theming";
+import { withTheme } from "react-native-paper/lib\\module\\core\\theming";
