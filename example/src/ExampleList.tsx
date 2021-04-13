9eeea3cf60a51d2ec51e874de5a5b07065d4beaa
lukewalczak <lukasz.walczak.pwr@gmail.com>
2021-04-13 13:44:41 +0200

    fix: correct example list background color

--- example/src/ExampleList.tsx
+++ example/src/ExampleList.tsx
@@ -109,6 +109,7 @@ export default function ExampleList({ navigation }: Props) {
         backgroundColor: colors.background,
         paddingBottom: safeArea.bottom,
       }}
+      style={{ backgroundColor: colors.background }}
       ItemSeparatorComponent={Divider}
       renderItem={renderItem}
       keyExtractor={keyExtractor}
