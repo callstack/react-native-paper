426c025074f9fe881ee1a5e31818dad9b35dc181
Bruno Castro <bruno.castro@codeminer42.com>
2021-05-12 14:41:45 -0300

    feat: add prop right to DrawerItem (#2505)

--- example/src/DrawerItems.tsx
+++ example/src/DrawerItems.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { View, StyleSheet, Platform } from 'react-native';
 import {
+  Badge,
   Drawer,
   Switch,
   TouchableRipple,
@@ -18,7 +19,18 @@ type Props = {
 
 const DrawerItemsData = [
   { label: 'Inbox', icon: 'inbox', key: 0 },
-  { label: 'Starred', icon: 'star', key: 1 },
+  {
+    label: 'Starred',
+    icon: 'star',
+    key: 1,
+    right: ({ color }: { color: string }) => (
+      <Badge
+        visible
+        size={8}
+        style={[styles.badge, { backgroundColor: color }]}
+      />
+    ),
+  },
   { label: 'Sent mail', icon: 'send', key: 2 },
   { label: 'Colored label', icon: 'palette', key: 3 },
   { label: 'A very long title that will be truncated', icon: 'delete', key: 4 },
@@ -81,6 +93,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingVertical: 12,
     paddingHorizontal: 16,
+  },
+  badge: {
+    alignSelf: 'center',
   },
 });
 
