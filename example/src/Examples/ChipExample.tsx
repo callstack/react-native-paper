8448593ad748d509efce62213db124ef53587b3b
Koussay Haj Kacem <29253287+essana3@users.noreply.github.com>
2021-10-25 17:58:17 +0100

    Add custom close icon for the Chip component (#2883)
    Co-authored-by: Koussay Haj Kacem <koussay.haj.kacem@kaoun.com>

--- example/src/Examples/ChipExample.tsx
+++ example/src/Examples/ChipExample.tsx
@@ -140,7 +140,6 @@ const ChipExample = () => {
             >
               With onLongPress
             </Chip>
-
             <Chip
               selected
               onPress={() => {}}
@@ -206,6 +205,15 @@ const ChipExample = () => {
               With a very big text: React Native Paper is a high-quality,
               standard-compliant Material Design library that has you covered in
               all major use-cases.
+            </Chip>
+            <Chip
+              onPress={() => {}}
+              onClose={() => {}}
+              closeIcon="arrow-down"
+              style={styles.chip}
+              closeIconAccessibilityLabel="Custom Close icon accessibility label"
+            >
+              With custom close icon
             </Chip>
           </View>
         </List.Section>
