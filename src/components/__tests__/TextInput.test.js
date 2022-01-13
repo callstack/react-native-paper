b216ae47700ff58af48e07dc59dedb16fa8aa955
lukewalczak <lukasz.walczak.pwr@gmail.com>
2022-01-13 17:57:42 +0100

    refactor: move manually the latest changes related to TextInput

--- src/components/__tests__/TextInput.test.js
+++ src/components/__tests__/TextInput.test.js
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, Platform } from 'react-native';
 import { fireEvent, render } from 'react-native-testing-library';
 import TextInput from '../TextInput/TextInput';
-import { areLabelsEqual } from '../TextInput/helpers';
-import { blue500, red500 } from '../../styles/colors';
+import { red500 } from '../../styles/colors';
 
 const style = StyleSheet.create({
   inputStyle: {
@@ -174,106 +173,4 @@ it('correctly applies a component as the text label', () => {
   );
 
   expect(toJSON()).toMatchSnapshot();
-});
-
-it('correctly compares labels when both are string', () => {
-  expect(areLabelsEqual('Comments', 'Comments')).toBe(true);
-  expect(areLabelsEqual('Comments', 'No Comment')).toBe(false);
-});
-
-it('correctly compares labels when one is string and one is a component', () => {
-  expect(areLabelsEqual(<Text>Comments</Text>, 'Comments')).toBe(false);
-});
-
-it('correctly compares labels when both labels are falsy', () => {
-  // We're treating all falsy values as equivalent
-  expect(areLabelsEqual()).toBe(true);
-  expect(areLabelsEqual(undefined, undefined)).toBe(true);
-  expect(areLabelsEqual(null, null)).toBe(true);
-  expect(areLabelsEqual(undefined, '')).toBe(true);
-  expect(areLabelsEqual(null, '')).toBe(true);
-  expect(areLabelsEqual(undefined, null)).toBe(true);
-});
-
-it('correctly compares labels when both labels are components', () => {
-  // Same component; same props, same children
-  const component1 = <Text>Comments</Text>;
-
-  let component2 = <Text>Comments</Text>;
-  expect(areLabelsEqual(component1, component2)).toBe(true);
-
-  // Same component; same props, different children
-  component2 = <Text>Another Comment</Text>;
-  expect(areLabelsEqual(component1, component2)).toBe(false);
-
-  // Different component; same props, same children
-  component2 = <View>Comments</View>;
-  expect(areLabelsEqual(component1, component2)).toBe(false);
-
-  // Same component; different props, same children
-  component2 = (
-    <Text multiline style={{ color: red500 }}>
-      Comments
-    </Text>
-  );
-  expect(areLabelsEqual(component1, component2)).toBe(false);
-});
-
-it('correctly compares labels for nested components', () => {
-  // Same component; same props, same children
-  const component1 = (
-    <Text>
-      <Text style={{ color: red500 }}>*</Text> Comments
-    </Text>
-  );
-
-  let component2 = (
-    <Text>
-      <Text style={{ color: red500 }}>*</Text> Comments
-    </Text>
-  );
-  expect(areLabelsEqual(component1, component2)).toBe(true);
-
-  // Same component; same props, different children
-  component2 = (
-    <Text>
-      <Text style={{ color: red500 }}>Comments</Text> continues
-    </Text>
-  );
-  expect(areLabelsEqual(component1, component2)).toBe(false);
-
-  // Different component; same props, same children
-  component2 = (
-    <View>
-      <Text style={{ color: red500 }}>*</Text> Comments
-    </View>
-  );
-  expect(areLabelsEqual(component1, component2)).toBe(false);
-
-  // Same component; different props, same children
-  component2 = (
-    <Text multiline>
-      <Text style={{ color: red500 }}>*</Text> Comments
-    </Text>
-  );
-  expect(areLabelsEqual(component1, component2)).toBe(false);
-
-  // Same component; same props, different number of children
-  component2 = (
-    <Text>
-      <Text style={{ color: red500 }}>*</Text>
-    </Text>
-  );
-  expect(areLabelsEqual(component1, component2)).toBe(false);
-
-  // Same component; different props in inner component, same children
-  component2 = (
-    <Text>
-      <Text multiline style={{ color: blue500 }}>
-        *
-      </Text>{' '}
-      Comments
-    </Text>
-  );
-  expect(areLabelsEqual(component1, component2)).toBe(false);
 });
