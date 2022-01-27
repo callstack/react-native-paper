fc817f98b6f32a21c5eb8c2e3aef10d97f7c1a96
Daniel Szczepanik <tmlm18@gmail.com>
2022-01-27 14:54:42 +0100

    chore: change imports in tests and default export names

--- src/core/theming.tsx
+++ src/core/theming.tsx
@@ -1,6 +1,6 @@
 import { createTheming } from '@callstack/react-theme-provider';
-import type { Theme } from '../types';
 import LightTheme from '../styles/themes/v2/LightTheme';
+import type { Theme } from '../types';
 
 export const { ThemeProvider, withTheme, useTheme } = createTheming<Theme>(
   LightTheme as Theme
