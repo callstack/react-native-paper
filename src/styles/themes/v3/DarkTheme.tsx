58683cc7322221e5b167dab49e18fb6237947429
Daniel Szczepanik <tmlm18@gmail.com>
2022-02-15 12:17:41 +0100

    chore: update themes with elevation props

--- src/styles/themes/v3/DarkTheme.tsx
+++ src/styles/themes/v3/DarkTheme.tsx
@@ -44,6 +44,46 @@ const DarkTheme: ThemeBase = {
     inverseOnSurface: palette.neutral90,
     inverseSurface: palette.neutral20,
     inversePrimary: palette.primary40,
+    elevation: {
+      level0: palette.primary0,
+      // Hard-coded values assuming palette.primary99 is used as background for colors with opacity
+      // Opaque rgba values cause RN to transfer shadows to children nodes
+      // instead of View component in Surface. Providing solid background fixes the issue
+      //
+      // level1: color(palette.primary80).alpha(0.05).rgb().string(),
+      // level2: color(palette.primary80).alpha(0.08).rgb().string(),
+      // level3: color(palette.primary80).alpha(0.11).rgb().string(),
+      // level4: color(palette.primary80).alpha(0.12).rgb().string(),
+      // level5: color(palette.primary80).alpha(0.14).rgb().string(),
+      level1: 'rgb(37, 35, 42)',
+      level2: 'rgb(44, 40, 49)',
+      level3: 'rgb(49, 44, 56)',
+      level4: 'rgb(51, 46, 58)',
+      level5: 'rgb(52, 49, 63)',
+    },
+    elevationShadows: {
+      level0: ['', ''],
+      level1: [
+        '0px 1px 2px rgba(0, 0, 0, 0.3)',
+        '0px 1px 3px rgba(0, 0, 0, 0.15)',
+      ],
+      level2: [
+        '0px 1px 2px rgba(0, 0, 0, 0.3)',
+        '0px 2px 6px rgba(0, 0, 0, 0.15)',
+      ],
+      level3: [
+        '0px 1px 3px rgba(0, 0, 0, 0.3)',
+        '0px 4px 8px rgba(0, 0, 0, 0.15)',
+      ],
+      level4: [
+        '0px 2px 3px rgba(0, 0, 0, 0.3)',
+        '0px 6px 10px rgba(0, 0, 0, 0.15)',
+      ],
+      level5: [
+        '0px 4px 4px rgba(0, 0, 0, 0.3)',
+        '0px 8px 12px rgba(0, 0, 0, 0.15)',
+      ],
+    },
   },
 };
 
--- src/styles/themes/v3/LightTheme.tsx
+++ src/styles/themes/v3/LightTheme.tsx
@@ -46,6 +46,46 @@ const LightTheme: ThemeBase = {
     inverseOnSurface: palette.neutral20,
     inverseSurface: palette.neutral95,
     inversePrimary: palette.primary80,
+    elevation: {
+      level0: palette.primary99,
+      // Hard-coded values assuming palette.primary99 is used as background for colors with opacity
+      // Opaque rgba values cause RN to transfer shadows to children nodes
+      // instead of View component in Surface. Providing solid background fixes the issue
+      //
+      // level1: color(palette.primary40).alpha(0.05).rgb().string(),
+      // level2: color(palette.primary40).alpha(0.08).rgb().string(),
+      // level3: color(palette.primary40).alpha(0.11).rgb().string(),
+      // level4: color(palette.primary40).alpha(0.12).rgb().string(),
+      // level5: color(palette.primary40).alpha(0.14).rgb().string(),
+      level1: 'rgb(247, 243, 249)',
+      level2: 'rgb(243, 237, 246)',
+      level3: 'rgb(238, 232, 244)',
+      level4: 'rgb(236, 230, 243)',
+      level5: 'rgb(233, 227, 241)',
+    },
+    elevationShadows: {
+      level0: ['', ''],
+      level1: [
+        '0px 1px 3px rgba(0, 0, 0, 0.15)',
+        '0px 1px 2px rgba(0, 0, 0, 0.3)',
+      ],
+      level2: [
+        '0px 2px 6px rgba(0, 0, 0, 0.15)]',
+        '0px 1px 2px rgba(0, 0, 0, 0.3)',
+      ],
+      level3: [
+        '0px 1px 3px rgba(0, 0, 0, 0.3)',
+        '0px 4px 8px rgba(0, 0, 0, 0.15)',
+      ],
+      level4: [
+        '0px 2px 3px rgba(0, 0, 0, 0.3)',
+        '0px 6px 10px rgba(0, 0, 0, 0.15)',
+      ],
+      level5: [
+        '0px 4px 4px rgba(0, 0, 0, 0.3)',
+        '0px 8px 12px rgba(0, 0, 0, 0.15)',
+      ],
+    },
   },
   fonts: configureFonts(),
   animation: {
