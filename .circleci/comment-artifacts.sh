fec4a107f68b17aaee6952416b31809af2fe9364
lukewalczak <lukasz.walczak.pwr@gmail.com>
2022-06-05 17:55:21 +0200

    docs: migration guide

--- .circleci/comment-artifacts.sh
+++ .circleci/comment-artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL="https://callstack-github-bot.herokuapp.com/comment"
-LINK="https://$CIRCLE_BUILD_NUM-71323749-gh.circle-artifacts.com/$CIRCLE_NODE_INDEX/docs/index.html"
+LINK="https://output.circle-artifacts.com/output/job/$CIRCLE_WORKFLOW_JOB_ID/artifacts/$CIRCLE_NODE_INDEX/docs/index.html"
 TEMPLATE="Hey @{{user.login}}, thank you for your pull request 🤗. The documentation from this branch can be viewed [here]($LINK)."
 
 read -r -d '' DATA << EOM
