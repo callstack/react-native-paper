54f8101f842cf521261e72db9026e08c2391f91d
Luke Walczak <lukasz.walczak.pwr@gmail.com>
2022-06-14 23:30:58 +0200

    refactor: set theme v3 as default, update theming docs (#3208)

--- src/core/theming.tsx
+++ src/core/theming.tsx
@@ -1,5 +1,5 @@
 import { createTheming } from '@callstack/react-theme-provider';
-import LightTheme from '../styles/themes/v2/LightTheme';
+import LightTheme from '../styles/themes/v3/LightTheme';
 import type { Theme } from '../types';
 
 export const { ThemeProvider, withTheme, useTheme } = createTheming<Theme>(
