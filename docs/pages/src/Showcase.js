604c998734456ce8fd1cf1b923e683db4235a1f6
subnub <kyle.hoell@gmail.com>
2022-07-19 23:56:16 -0400

    docs: add homework helper to showcase

--- docs/pages/src/Showcase.js
+++ docs/pages/src/Showcase.js
@@ -140,6 +140,14 @@ const data: Data[] = [
     image: 'showcase/quakemap.png',
     android: 'https://play.google.com/store/apps/details?id=com.sal.quakemap',
   },
+  {
+    color: '#5222d0',
+    name: 'Homework Helper - Find HW Answers',
+    image: 'showcase/homeworkhelper.png',
+    ios: 'https://apps.apple.com/app/homework-helper-app/id1632652122',
+    android:
+      'https://play.google.com/store/apps/details?id=com.kylehoell.homeworkhelper',
+  },
 ];
 
 export default class Showcase extends React.Component<{}> {
