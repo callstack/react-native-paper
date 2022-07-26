35fd84f284bbaaf3a64eecdb901e85cfe0033c22
Daniel Szczepanik <tmlm18@gmail.com>
2022-07-26 09:30:02 +0200

    feat: expose default theme

--- src/babel/__fixtures__/rewrite-imports/output.js
+++ src/babel/__fixtures__/rewrite-imports/output.js
@@ -1,5 +1,4 @@
 /* eslint-disable prettier/prettier */
-import { Text } from 'react-native';
 import PaperProvider from "react-native-paper/lib/module/core/Provider";
 import BottomNavigation from "react-native-paper/lib/module/components/BottomNavigation/BottomNavigation";
 import Button from "react-native-paper/lib/module/components/Button/Button";
@@ -7,6 +6,7 @@ import FAB from "react-native-paper/lib/module/components/FAB";
 import Appbar from "react-native-paper/lib/module/components/Appbar";
 import * as MD2Colors from "react-native-paper/lib/module/styles/themes/v2/colors";
 import { MD3Colors } from "react-native-paper/lib/module/styles/themes/v3/tokens";
-import { NonExistent, NonExistentSecond as Stuff, InternalTheme } from "react-native-paper/lib/module/index.js";
+import { NonExistent, NonExistentSecond as Stuff } from "react-native-paper/lib/module/index.js";
 import { ThemeProvider } from "react-native-paper/lib/module/core/theming";
 import { withTheme } from "react-native-paper/lib/module/core/theming";
+import { DefaultTheme } from "react-native-paper/lib/module/core/theming";
