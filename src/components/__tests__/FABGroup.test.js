806838d7eb593e79063fe67bae5321124d5efd91
Tarik Github <tarikdotcom@gmail.com>
2022-09-08 02:43:31 +0300

    test: unit tests for FABGroup component, labelStyle, containerStyle

--- src/components/__tests__/FABGroup.test.js
+++ src/components/__tests__/FABGroup.test.js
@@ -1,6 +1,9 @@
 import color from 'color';
-import { getFABGroupColors } from '../FAB/utils';
+import * as React from 'react';
+import renderer from 'react-test-renderer';
 import { getTheme } from '../../core/theming';
+import { getFABGroupColors } from '../FAB/utils';
+import FAB from '../FAB';
 
 describe('getFABGroupColors - backdrop color', () => {
   it('should return custom color', () => {
@@ -92,5 +95,49 @@ describe('getFABGroupColors - stacked FAB background color', () => {
     ).toMatchObject({
       stackedFABBackgroundColor: getTheme(false, false).colors.surface,
     });
+  });
+});
+
+describe('FABActions - labelStyle - containerStyle', () => {
+  it('renders actions with custom label style', () => {
+    const tree = renderer
+      .create(
+        <FAB.Group
+          visible
+          open
+          icon="filter"
+          actions={[
+            {
+              label: 'test',
+              icon: 'arrow-up',
+              labelStyle: { fontSize: 24, fontWeight: '500' },
+            },
+          ]}
+        />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders actions with custom container style', () => {
+    const tree = renderer
+      .create(
+        <FAB.Group
+          visible
+          open
+          icon="filter"
+          actions={[
+            {
+              label: 'test',
+              icon: 'arrow-up',
+              containerStyle: { padding: 16, backgroundColor: 'royalblue' },
+            },
+          ]}
+        />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
