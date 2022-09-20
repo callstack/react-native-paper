ea22a49a4de567913a9429db0d13474b278b9cc0
lukewalczak <lukasz.walczak.pwr@gmail.com>
2022-09-20 16:32:37 +0200

    refactor: customize android navigation animation to avoid flash

--- example/src/RootNavigator.tsx
+++ example/src/RootNavigator.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
 import { Appbar } from 'react-native-paper';
 import type { DrawerNavigationProp } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from '@react-navigation/stack';
 import { getHeaderTitle } from '@react-navigation/elements';
 import ExampleList, { examples } from './ExampleList';
+import { Platform } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function Root() {
+  const cardStyleInterpolator =
+    Platform.OS === 'android'
+      ? CardStyleInterpolators.forFadeFromBottomAndroid
+      : CardStyleInterpolators.forHorizontalIOS;
   return (
     <Stack.Navigator
       screenOptions={({ navigation }) => {
         return {
           detachPreviousScreen: !navigation.isFocused(),
+          cardStyleInterpolator,
           header: ({ navigation, route, options, back }) => {
             const title = getHeaderTitle(options, route.name);
             return (
@@ -40,14 +49,18 @@ export default function Root() {
       <Stack.Screen
         name="ExampleList"
         component={ExampleList}
-        options={{ title: 'Examples' }}
+        options={{
+          title: 'Examples',
+        }}
       />
       {(Object.keys(examples) as Array<keyof typeof examples>).map((id) => (
         <Stack.Screen
           key={id}
           name={id}
           component={examples[id]}
-          options={{ title: examples[id].title }}
+          options={{
+            title: examples[id].title,
+          }}
         />
       ))}
     </Stack.Navigator>
