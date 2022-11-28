5466a530937e3c5c06c1f9ac224d44c6aaa1e813
lukewalczak <lukasz.walczak.pwr@gmail.com>
2022-11-28 15:18:40 +0100

    refactor: correct segmented buttons example code

--- src/components/SegmentedButtons/SegmentedButtons.tsx
+++ src/components/SegmentedButtons/SegmentedButtons.tsx
@@ -85,29 +85,39 @@ export type Props = {
  * ## Usage
  * ```js
  * import * as React from 'react';
+ * import { SafeAreaView, StyleSheet } from 'react-native';
  * import { SegmentedButtons } from 'react-native-paper';
  *
  * const MyComponent = () => {
  *   const [value, setValue] = React.useState('');
  *
  *   return (
- *     <SegmentedButtons
- *      value={value}
- *      onValueChange={setValue}
- *      buttons={[
- *        {
- *          value: 'walk',
- *          label: 'Walking',
- *        },
- *        {
- *          value: 'train',
- *          label: 'Transit',
- *        },
- *      ]}
- *      style={styles.group}
- *    />
+ *     <SafeAreaView style={styles.container}>
+ *       <SegmentedButtons
+ *         value={value}
+ *         onValueChange={setValue}
+ *         buttons={[
+ *           {
+ *             value: 'walk',
+ *             label: 'Walking',
+ *           },
+ *           {
+ *             value: 'train',
+ *             label: 'Transit',
+ *           },
+ *           { value: 'drive', label: 'Driving' },
+ *         ]}
+ *       />
+ *     </SafeAreaView>
  *   );
  * };
+ *
+ * const styles = StyleSheet.create({
+ *   container: {
+ *     flex: 1,
+ *     alignItems: 'center',
+ *   },
+ * });
  *
  * export default MyComponent;
  *```
