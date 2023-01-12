235e4f826507a6353f1e94d1eafdea7147435526
Edu <eduardo.graciano@callstack.com>
2023-01-12 22:33:50 +0100

    fix: updated get colors for icon and label

--- src/components/BottomNavigation/utils.ts
+++ src/components/BottomNavigation/utils.ts
@@ -3,7 +3,7 @@ import type { InternalTheme } from 'src/types';
 
 import type { black, white } from '../../styles/themes/v2/colors';
 
-type BaseConfig = {
+type BaseProps = {
   defaultColor: typeof black | typeof white;
   theme: InternalTheme;
 };
@@ -12,28 +12,36 @@ export const getActiveTintColor = ({
   activeColor,
   defaultColor,
   theme,
-}: {
+}: BaseProps & {
   activeColor: string | undefined;
-} & BaseConfig) => {
-  return activeColor
-    ? activeColor
-    : theme.isV3
-    ? theme.colors.onSecondaryContainer
-    : defaultColor;
+}) => {
+  if (typeof activeColor === 'string') {
+    return activeColor;
+  }
+
+  if (theme.isV3) {
+    return theme.colors.onSecondaryContainer;
+  }
+
+  return defaultColor;
 };
 
 export const getInactiveTintColor = ({
   inactiveColor,
   defaultColor,
   theme,
-}: {
+}: BaseProps & {
   inactiveColor: string | undefined;
-} & BaseConfig) => {
-  return inactiveColor
-    ? inactiveColor
-    : theme.isV3
-    ? theme.colors.onSurfaceVariant
-    : color(defaultColor).alpha(0.5).rgb().string();
+}) => {
+  if (typeof inactiveColor === 'string') {
+    return inactiveColor;
+  }
+
+  if (theme.isV3) {
+    return theme.colors.onSurfaceVariant;
+  }
+
+  return color(defaultColor).alpha(0.5).rgb().string();
 };
 
 export const getLabelColor = ({
@@ -42,16 +50,22 @@ export const getLabelColor = ({
   focused,
   defaultColor,
   theme,
-}: {
+}: BaseProps & {
   tintColor: string;
   hasColor: boolean;
   focused: boolean;
-} & BaseConfig) => {
-  return hasColor
-    ? tintColor
-    : focused
-    ? theme.colors.onSurface
-    : theme.isV3
-    ? theme.colors.onSurfaceVariant
-    : defaultColor;
+}) => {
+  if (hasColor) {
+    return tintColor;
+  }
+
+  if (focused) {
+    return theme.colors.onSurface;
+  }
+
+  if (theme.isV3) {
+    return theme.colors.onSurfaceVariant;
+  }
+
+  return defaultColor;
 };
