77ef516177775e5d0853ced6d3550e623203ce56
Yaman Katby <m.yaman.katby@gmail.com>
2023-02-06 14:12:21 +0300

    refactor: remark-preview-plugin

--- docusaurus/plugins/remark-preview-plugin.js
+++ docusaurus/plugins/remark-preview-plugin.js
@@ -1,49 +1,31 @@
-const path = require('path');
-const fs = require('fs');
-const fsPromise = require('fs/promises');
-
-const previewsDir = 'src/__previews__';
-const previewsDirPath = path.join(__dirname, '..', previewsDir);
-
-const processNode = async (node, i, parent) => {
-  const nextNode = parent.children[i + 1];
-  const hasTsNode = nextNode?.lang === 'tsx';
-
-  const nodesToInsert = [
-    {
-      type: 'jsx',
-      value: `<Preview jsCode={\`${node.value.replaceAll('`', '\\`')}\`} />`,
-    },
-  ];
-
-  node.meta = node.meta.replace('preview', '');
-
-  parent.children.splice(i, hasTsNode ? 2 : 1, ...nodesToInsert);
-};
-
 module.exports = () => {
   return async (tree) => {
     const { visit } = await import('unist-util-visit');
 
-    if (!fs.existsSync(previewsDirPath)) {
-      await fsPromise.mkdir(previewsDirPath);
-    }
-
-    const nodesToProcess = [];
+    let needsImport = false;
 
     visit(tree, 'code', (node, i, parent) => {
       if (node.meta?.includes('preview')) {
-        nodesToProcess.push(processNode(node, i, parent));
+        needsImport = true;
+
+        const nextNode = parent.children[i + 1];
+        const hasTsNode = nextNode?.lang === 'tsx';
+
+        const jsCode = node.value.replaceAll('`', '\\`');
+
+        const nodesToInsert = [
+          { type: 'jsx', value: `<Preview jsCode={\`${jsCode}\`} />` },
+        ];
+
+        parent.children.splice(i, hasTsNode ? 2 : 1, ...nodesToInsert);
       }
     });
 
-    if (nodesToProcess.length > 0) {
+    if (needsImport) {
       tree.children.unshift({
         type: 'import',
         value: `import Preview from '@site/src/components/preview';`,
       });
-
-      await Promise.all(nodesToProcess);
     }
 
     return tree;
