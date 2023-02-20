4e578ba89e38b1c00ee98770ee7e8485035065ae
Yaman Katby <m.yaman.katby@gmail.com>
2023-02-20 17:07:09 +0300

    fix: update the editUrl in docusaurus config file

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -178,7 +178,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/callstack/react-native-paper/tree/main/docs/',
           remarkPlugins: [
             [require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }],
           ],
@@ -188,7 +188,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/callstack/react-native-paper/tree/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
