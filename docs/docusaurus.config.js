cf5bb19e71940e260c08870b66d8b656305397e4
Daniel Szczepanik <tmlm18@gmail.com>
2023-02-20 12:41:39 +0100

    chore: get rid of old docs, replace with docusaurus (#3691)
    Co-authored-by: lukewalczak <lukasz.walczak.pwr@gmail.com>
    Co-authored-by: Andriy Tsaryov <andriy.tsaryov@gmail.com>
    Co-authored-by: Yaman KATBY <m.yaman.katby@gmail.com>

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,287 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const path = require('path');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+
+const { NODE_ENV, DOCUSAURUS_BASE_URL } = process.env;
+
+const title = 'React Native Paper';
+
+const url = 'https://callstack.github.io';
+
+/**
+ * There are 3 environments that we want to support:
+ * 1. Development - Docs should work on `http://localhost:3000/`
+ * 2. Production - Docs should work on `https://callstack.github.io/react-native-paper/
+ * 3. CI - CircleCI builds and stores preview artifacts.
+ *    Docs should work on `https://output.circle-artifacts.com/output/job/$CIRCLE_WORKFLOW_JOB_ID/artifacts/$CIRCLE_NODE_INDEX/`
+ *
+ * Both `baseUrl` and `publicUrl` work together to ensure assets and links work properly on every deployment
+ */
+const baseUrl =
+  DOCUSAURUS_BASE_URL ||
+  (NODE_ENV === 'development' ? '/' : '/react-native-paper/');
+const publicUrl =
+  NODE_ENV === 'development' ? 'http://localhost:3000/' : `${url}${baseUrl}`;
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title,
+  staticDirectories: ['public', 'static'],
+  tagline: 'Material Design for React Native',
+  url,
+  baseUrl,
+  onBrokenLinks: 'log',
+  onBrokenMarkdownLinks: 'log',
+  favicon: 'images/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'callstack', // Usually your GitHub org/user name.
+  projectName: 'react-native-paper', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  plugins: [
+    [
+      './component-docs-plugin',
+      {
+        docsRootDir: path.join(__dirname, 'docs', 'components'),
+        libsRootDir: path.join(__dirname, '..', 'src', 'components'),
+        pages: {
+          ActivityIndicator: 'ActivityIndicator',
+          AnimatedFAB: 'FAB/AnimatedFAB',
+          Appbar: {
+            Appbar: 'Appbar/Appbar',
+            AppbarAction: 'Appbar/AppbarAction',
+            AppbarBackAction: 'Appbar/AppbarBackAction',
+            AppbarContent: 'Appbar/AppbarContent',
+            AppbarHeader: 'Appbar/AppbarHeader',
+          },
+          Avatar: {
+            AvatarIcon: 'Avatar/AvatarIcon',
+            AvatarImage: 'Avatar/AvatarImage',
+            AvatarText: 'Avatar/AvatarText',
+          },
+          Badge: 'Badge',
+          Banner: 'Banner',
+          BottomNavigation: 'BottomNavigation/BottomNavigation',
+          Button: 'Button/Button',
+          Card: {
+            Card: 'Card/Card',
+            CardActions: 'Card/CardActions',
+            CardContent: 'Card/CardContent',
+            CardCover: 'Card/CardCover',
+            CardTitle: 'Card/CardTitle',
+          },
+          Checkbox: {
+            Checkbox: 'Checkbox/Checkbox',
+            CheckboxAndroid: 'Checkbox/CheckboxAndroid',
+            CheckboxIOS: 'Checkbox/CheckboxIOS',
+            CheckboxItem: 'Checkbox/CheckboxItem',
+          },
+          Chip: 'Chip/Chip',
+          DataTable: {
+            DataTable: 'DataTable/DataTable',
+            DataTableCell: 'DataTable/DataTableCell',
+            DataTableHeader: 'DataTable/DataTableHeader',
+            DataTablePagination: 'DataTable/DataTablePagination',
+            DataTableRow: 'DataTable/DataTableRow',
+            DataTableTitle: 'DataTable/DataTableTitle',
+          },
+          Dialog: {
+            Dialog: 'Dialog/Dialog',
+            DialogActions: 'Dialog/DialogActions',
+            DialogContent: 'Dialog/DialogContent',
+            DialogIcon: 'Dialog/DialogIcon',
+            DialogScrollArea: 'Dialog/DialogScrollArea',
+            DialogTitle: 'Dialog/DialogTitle',
+          },
+          Divider: 'Divider',
+          Drawer: {
+            DrawerCollapsedItem: 'Drawer/DrawerCollapsedItem',
+            DrawerItem: 'Drawer/DrawerItem',
+            DrawerSection: 'Drawer/DrawerSection',
+          },
+          FAB: {
+            FAB: 'FAB/FAB',
+            FABGroup: 'FAB/FABGroup',
+          },
+          HelperText: 'HelperText',
+          IconButton: 'IconButton/IconButton',
+          List: {
+            ListAccordion: 'List/ListAccordion',
+            ListAccordionGroup: 'List/ListAccordionGroup',
+            ListIcon: 'List/ListIcon',
+            ListItem: 'List/ListItem',
+            ListSection: 'List/ListSection',
+            ListSubheader: 'List/ListSubheader',
+          },
+          Menu: {
+            Menu: 'Menu/Menu',
+            MenuItem: 'Menu/MenuItem',
+          },
+          Modal: 'Modal',
+          Portal: {
+            Portal: 'Portal/Portal',
+            PortalHost: 'Portal/PortalHost',
+          },
+          ProgressBar: 'ProgressBar',
+          RadioButton: {
+            RadioButton: 'RadioButton/RadioButton',
+            RadioButtonAndroid: 'RadioButton/RadioButtonAndroid',
+            RadioButtonGroup: 'RadioButton/RadioButtonGroup',
+            RadioButtonIOS: 'RadioButton/RadioButtonIOS',
+            RadioButtonItem: 'RadioButton/RadioButtonItem',
+          },
+          Searchbar: 'Searchbar',
+          SegmentedButtons: 'SegmentedButtons/SegmentedButtons',
+          Snackbar: 'Snackbar',
+          Surface: 'Surface',
+          Switch: 'Switch/Switch',
+          TextInput: {
+            TextInput: 'TextInput/TextInput',
+            TextInputAffix: 'TextInput/Adornment/TextInputAffix',
+            TextInputIcon: 'TextInput/Adornment/TextInputIcon',
+          },
+          ToggleButton: {
+            ToggleButton: 'ToggleButton/ToggleButton',
+            ToggleButtonGroup: 'ToggleButton/ToggleButtonGroup',
+            ToggleButtonRow: 'ToggleButton/ToggleButtonRow',
+          },
+          Tooltip: 'Tooltip/Tooltip',
+          TouchableRipple: 'TouchableRipple/TouchableRipple',
+          Typography: {
+            Text: 'Typography/Text',
+          },
+        },
+      },
+    ],
+    require('./plugins/docusaurus-react-native-plugin'),
+  ],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          remarkPlugins: [
+            [require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }],
+          ],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        logo: {
+          alt: 'React Native Paper',
+          src: 'images/sidebar-logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'guides/getting-started',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            type: 'doc',
+            docId: 'components/ActivityIndicator',
+            position: 'left',
+            label: 'Components',
+          },
+          {
+            type: 'doc',
+            docId: 'showcase',
+            position: 'left',
+            label: 'Showcase',
+          },
+          {
+            type: 'dropdown',
+            label: 'v5.x',
+            position: 'right',
+            items: [
+              {
+                label: 'v4.x',
+                href: `${publicUrl}4.0/`,
+              },
+              {
+                label: 'v3.x',
+                href: `${publicUrl}3.0/`,
+              },
+              {
+                label: 'v2.x',
+                href: `${publicUrl}2.0/`,
+              },
+              {
+                label: 'v1.x',
+                href: `${publicUrl}1.0/`,
+              },
+            ],
+          },
+          {
+            href: 'https://discord.gg/zwR2Cdh',
+            className: 'nav-link nav-discord-link',
+            'aria-label': 'Discord',
+            position: 'right',
+          },
+          {
+            href: 'https://twitter.com/rn_paper',
+            className: 'nav-link nav-twitter-link',
+            'aria-label': 'Twitter',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/callstack/react-native-paper',
+            className: 'nav-link nav-github-link',
+            'aria-label': 'GitHub',
+            position: 'right',
+          },
+        ],
+        title,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+
+  themes: [
+    [
+      '@easyops-cn/docusaurus-search-local',
+      {
+        hashed: true,
+      },
+    ],
+  ],
+};
+
+module.exports = config;
---
+++ docs/src/components/Showcase.tsx
@@ -0,0 +1,220 @@
+import React from 'react';
+
+import color from 'color';
+
+import AppStoreIcon from './AppStoreIcon';
+import GithubIcon from './GithubIcon';
+import GooglePlayIcon from './GooglePlayIcon';
+
+type Data = {
+  color: string;
+  name: string;
+  image: string;
+  android?: string;
+  ios?: string;
+  github?: string;
+};
+
+const data: Data[] = [
+  {
+    color: '#6200EE',
+    name: 'Paper Example App',
+    image: '../showcase/paper.png',
+    android:
+      'https://play.google.com/store/apps/details?id=com.callstack.reactnativepaperexample',
+    ios: 'https://apps.apple.com/app/react-native-paper/id1548934513',
+    github: 'https://github.com/callstack/react-native-paper/tree/main/example',
+  },
+  {
+    color: '#696AC3',
+    name: 'YumMeals',
+    image: '../showcase/yummeals.png',
+    github: 'https://github.com/BernStrom/YumMeals',
+  },
+  {
+    color: '#673AB7',
+    name: 'Showman',
+    image: '../showcase/showman.png',
+  },
+  {
+    color: '#F73942',
+    name: 'AppnGO',
+    image: '../showcase/appngo.png',
+    android:
+      'https://play.google.com/store/apps/details?id=com.helsedesign.appngo',
+    ios: 'https://itunes.apple.com/us/app/appngo/id1439585129',
+  },
+  {
+    color: '#fff',
+    name: 'Dark Hacker News',
+    image: '../showcase/darkhackernews.png',
+    ios: 'https://itunes.apple.com/us/app/dark-hacker-news/id1459946382?mt=8',
+  },
+  {
+    color: '#0ba360',
+    name: 'PandaDeals',
+    image: '../showcase/pandadeals.png',
+    android:
+      'https://play.google.com/store/apps/details?id=com.mattkoboski.couponsapp',
+    ios: 'https://apps.apple.com/pl/app/pandadeals/id1468755918',
+  },
+  {
+    color: '#A11E1E',
+    name: 'RaceCalendar',
+    image: '../showcase/racecalendar.png',
+    android:
+      'https://play.google.com/store/apps/details?id=in.micy.racecalendar',
+    ios: 'https://apps.apple.com/us/app/race-calendar/id1481539104',
+  },
+  {
+    color: '#4439A1',
+    name: 'Unicore',
+    image: '../showcase/unicore.png',
+    android: 'https://play.google.com/store/apps/details?id=com.atude.mywam',
+  },
+  {
+    color: '#19402E',
+    name: 'TracksNZ',
+    image: '../showcase/tracksnz.png',
+    android: 'https://play.google.com/store/apps/details?id=in.micy.tracksnz',
+    ios: 'https://apps.apple.com/nz/app/tracks-nz/id1488245855',
+  },
+  {
+    color: '#b985fc',
+    name: 'Astrale',
+    image: '../showcase/astrale.png',
+    github: 'https://github.com/jvidalv/astrale',
+    android: 'https://play.google.com/store/apps/details?id=josep.astrale',
+  },
+  {
+    color: '#29337a',
+    name: 'Lyra Collect',
+    image: '../showcase/lyracollect.png',
+    android:
+      'https://play.google.com/store/apps/details?id=com.lyra.lyracollect',
+    ios: 'https://apps.apple.com/fr/app/lyra-collect/id1469527737',
+  },
+  {
+    color: '#673AB7',
+    name: 'Rozy - Stay connected',
+    image: '../showcase/rozy.png',
+    ios: 'https://apps.apple.com/us/app/rozy/id1511264722',
+    android: 'https://play.google.com/store/apps/details?id=com.rozy_app.rozy',
+  },
+  {
+    color: '#CC0E00',
+    name: 'Zoomapto - Find Nearby Restaurants',
+    image: '../showcase/zoomapto.png',
+    github: 'https://github.com/frederic11/Zoomapto',
+    android:
+      'https://play.google.com/store/apps/details?id=com.frederictech.zoomapto',
+  },
+  {
+    color: '#1DE9B6',
+    name: 'Groovy - Build Habits Together',
+    image: '../showcase/groovy.png',
+    ios: 'https://apps.apple.com/us/app/groovy-set-your-goals/id1562352498',
+    android:
+      'https://play.google.com/store/apps/details?id=com.gabrieldierks.groovy',
+  },
+  {
+    color: '#673AB7',
+    name: 'Vibemap - Find Your Vibe',
+    image: '../showcase/vibemap.png',
+    ios: 'https://apps.apple.com/us/app/vibemap/id1496385897#?platform=iphone',
+    android:
+      'https://play.google.com/store/apps/details?id=com.vibemap.hotspots',
+  },
+  {
+    color: '#6200EE',
+    name: 'Clutch - Transmission Remote',
+    image: '../showcase/clutch.png',
+    android: 'https://play.google.com/store/apps/details?id=eu.tmdpw.clutch',
+  },
+  {
+    color: '#111827',
+    name: 'Quakemap',
+    image: '../showcase/quakemap.png',
+    android: 'https://play.google.com/store/apps/details?id=com.sal.quakemap',
+  },
+  {
+    color: '#5222d0',
+    name: 'Homework Helper - Find HW Answers',
+    image: '../showcase/homeworkhelper.png',
+    ios: 'https://apps.apple.com/app/homework-helper-app/id1632652122',
+    android:
+      'https://play.google.com/store/apps/details?id=com.kylehoell.homeworkhelper',
+  },
+  {
+    color: '#E1352F',
+    name: 'CrazyThursday',
+    image: '../showcase/crazythursday.jpg',
+    github: 'https://github.com/shensven/Crazy-Thursday',
+    android:
+      'https://play.google.com/store/apps/details?id=com.shensven.crazythursday',
+  },
+  {
+    color: '#566193',
+    name: 'Moon Meet',
+    image: '../showcase/moonmeet.png',
+    github: 'https://github.com/MoonMeet/MoonMeet-CrossPlatform',
+  },
+];
+
+export default function Showcase() {
+  return (
+    <div className="showcase-gallery">
+      {data.map((item) => {
+        const tintColor = color(item.color).isLight() ? '#000000' : '#FFFFFF';
+        return (
+          <div key={item.image}>
+            <div className="showcase-image-container">
+              <img className="showcase-image" src={item.image} alt="" />
+              <div
+                className="showcase-info"
+                style={{ backgroundColor: item.color }}
+              >
+                <h3
+                  className="showcase-app-name"
+                  style={{
+                    color: tintColor,
+                  }}
+                >
+                  {item.name}
+                </h3>
+                <div className="showcase-badge-container">
+                  <a
+                    href={item.android}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ opacity: item.android ? 1 : 0.4 }}
+                  >
+                    <GooglePlayIcon color={tintColor} />
+                  </a>
+                  <div className="showcase-separation" />
+                  <a
+                    href={item.ios}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ opacity: item.ios ? 1 : 0.4 }}
+                  >
+                    <AppStoreIcon color={tintColor} />
+                  </a>
+                  <div className="showcase-separation" />
+                  <a
+                    href={item.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ opacity: item.github ? 1 : 0.4 }}
+                  >
+                    <GithubIcon color={tintColor} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
