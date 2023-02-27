641f27de9d05430b12486d56cc6d3cbb207f3264
Krystyna Łosieczka <krysia@agabox.net>
2023-02-27 20:35:15 +0100

    fix: add missing babel plugin for expo web support

--- example/babel.config.js
+++ example/babel.config.js
@@ -23,6 +23,7 @@ module.exports = function (api) {
           },
         },
       ],
+      ['@babel/plugin-proposal-export-namespace-from'],
       ['react-native-reanimated/plugin'],
     ],
   };
