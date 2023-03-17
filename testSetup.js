92b5b3a82920cf660334a56cb9406e8d77f20beb
pac-guerreiro <>
2023-03-17 18:09:17 +0000

    chore: update tooltip tests for native and web

--- testSetup.js
+++ testSetup.js
@@ -1,3 +1,7 @@
 jest.useFakeTimers();
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useRoute: () => ({}),
+  useNavigation: () => ({ setParams: jest.fn() }),
+}));
