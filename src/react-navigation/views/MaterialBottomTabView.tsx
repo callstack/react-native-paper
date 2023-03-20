1ebbfe98ae8773c14ff501e01be5319e901f322a
josemak25 <amakirij@gmail.com>
2023-03-20 17:27:01 +0100

    fix: add return statement to prevent default

--- src/react-navigation/views/MaterialBottomTabView.tsx
+++ src/react-navigation/views/MaterialBottomTabView.tsx
@@ -58,7 +58,7 @@ export default function MaterialBottomTabView({
       });
 
       if (event.defaultPrevented) {
-        preventDefault();
+        return preventDefault();
       }
 
       callback?.(route);
