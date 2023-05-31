1ce31113cd67e7aa450c72d6f86cd4a20c8157a1
lukewalczak <lukasz.walczak.pwr@gmail.com>
2023-05-31 22:10:05 +0200

    docs: add theme colors for more components

--- docs/src/data/themeColors.js
+++ docs/src/data/themeColors.js
@@ -7,51 +7,63 @@ const themeColors = {
   Badge: {
     '-': {
       backgroundColor: 'theme.colors.error',
-      color: 'theme.colors.onError',
+      textColor: 'theme.colors.onError',
     },
   },
   Button: {
     active: {
       elevated: {
         backgroundColor: 'theme.colors.elevation.level1',
-        color: 'theme.colors.primary',
+        textColor: 'theme.colors.primary',
       },
       contained: {
         backgroundColor: 'theme.colors.primary',
-        color: 'theme.colors.onPrimary',
+        textColor: 'theme.colors.onPrimary',
       },
       'contained-tonal': {
         backgroundColor: 'theme.colors.secondaryContainer',
-        color: 'theme.colors.onSecondaryContainer',
+        textColor: 'theme.colors.onSecondaryContainer',
       },
       outlined: {
-        color: 'theme.colors.primary',
+        textColor: 'theme.colors.primary',
         borderColor: 'theme.colors.outline',
       },
       text: {
-        color: 'theme.colors.primary',
+        textColor: 'theme.colors.primary',
       },
     },
     disabled: {
       elevated: {
         backgroundColor: 'theme.colors.surfaceDisabled',
-        color: 'theme.colors.onSurfaceDisabled',
+        textColor: 'theme.colors.onSurfaceDisabled',
       },
       contained: {
         backgroundColor: 'theme.colors.surfaceDisabled',
-        color: 'theme.colors.onSurfaceDisabled',
+        textColor: 'theme.colors.onSurfaceDisabled',
       },
       'contained-tonal': {
         backgroundColor: 'theme.colors.surfaceDisabled',
-        color: 'theme.colors.onSurfaceDisabled',
+        textColor: 'theme.colors.onSurfaceDisabled',
       },
       outlined: {
-        color: 'theme.colors.onSurfaceDisabled',
+        textColor: 'theme.colors.onSurfaceDisabled',
         borderColor: 'theme.colors.surfaceDisabled',
       },
       text: {
-        color: 'theme.colors.onSurfaceDisabled',
+        textColor: 'theme.colors.onSurfaceDisabled',
       },
+    },
+  },
+  Card: {
+    contained: {
+      backgroundColor: 'theme.colors.surfaceVariant',
+    },
+    elevated: {
+      backgroundColor: 'theme.colors.elevation.level1',
+    },
+    outlined: {
+      backgroundColor: 'theme.colors.surface',
+      borderColor: 'theme.colors.outline',
     },
   },
   Dialog: {
@@ -67,6 +79,36 @@ const themeColors = {
   'Dialog.ScrollArea': {
     '-': {
       borderColor: 'theme.colors.surfaceVariant',
+    },
+  },
+  Divider: {
+    '-': {
+      dividerColor: 'theme.colors.outlineVariant',
+    },
+  },
+  'Drawer.CollapsedItem': {
+    active: {
+      backgroundColor: 'theme.colors.secondaryContainer',
+      textColor: 'theme.colors.onSurface',
+      iconColor: 'theme.colors.onSecondaryContainer',
+    },
+    inactive: {
+      textColor: 'theme.colors.onSurfaceVariant',
+      iconColor: 'theme.colors.onSurfaceVariant',
+    },
+  },
+  'Drawer.Item': {
+    active: {
+      backgroundColor: 'theme.colors.secondaryContainer',
+      'iconColor/textColor': 'theme.colors.onSecondaryContainer',
+    },
+    inactive: {
+      'iconColor/textColor': 'theme.colors.onSurfaceVariant',
+    },
+  },
+  'Drawer.Section': {
+    '-': {
+      titleColor: 'theme.colors.onSurfaceVariant',
     },
   },
   IconButton: {
@@ -123,17 +165,42 @@ const themeColors = {
       },
     },
   },
-
-  Card: {
-    contained: {
-      backgroundColor: 'theme.colors.surfaceVariant',
+  Modal: {
+    '-': {
+      backgroundColor: 'theme.colors.backdrop',
     },
-    elevated: {
-      backgroundColor: 'theme.colors.elevation.level1',
+  },
+  ProgressBar: {
+    '-': {
+      tintColor: 'theme.colors.primary',
+      trackTintColor: 'theme.colors.surfaceVariant',
     },
-    outlined: {
-      backgroundColor: 'theme.colors.surface',
-      borderColor: 'theme.colors.outline',
+  },
+  Searchbar: {
+    '-': {
+      backgroundColor: 'theme.colors.elevation.level3',
+      placeholderTextColor: 'theme.colors.onSurface',
+      textColor: 'theme.colors.onSurfaceVariant',
+      selectionColor: 'theme.colors.primary',
+      iconColor: 'theme.colors.onSurfaceVariant',
+      trailingIconColor: 'theme.colors.onSurfaceVariant',
+      dividerColor: 'theme.colors.outline',
+    },
+  },
+  Snackbar: {
+    '-': {
+      iconColor: 'theme.colors.inverseOnSurface',
+    },
+  },
+  Text: {
+    '-': {
+      textColor: 'theme.colors.onSurface',
+    },
+  },
+  Tooltip: {
+    '-': {
+      backgroundColor: 'theme.colors.onSurface',
+      textColor: 'theme.colors.surface',
     },
   },
 };
