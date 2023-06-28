4664a1cc60bfd4fd52bc23c2d2e9140f5d833a72
Luke Walczak <lukasz.walczak.pwr@gmail.com>
2023-06-28 20:26:29 +0200

    refactor: typography text styles adjustments (#3947)

--- src/components/Typography/Text.tsx
+++ src/components/Typography/Text.tsx
@@ -96,7 +96,43 @@ const Text = (
   }));
 
   if (theme.isV3 && variant) {
-    const font = theme.fonts[variant];
+    let font = theme.fonts[variant];
+    let textStyle = [font, style];
+
+    if (
+      rest.children &&
+      typeof rest.children === 'object' &&
+      'props' in rest.children
+    ) {
+      const { props } = rest.children;
+
+      // Context:   Some components have the built-in `Text` component with a predefined variant,
+      //            that also accepts `children` as a `React.Node`. This can result in a situation,
+      //            where another `Text` component is rendered within the built-in `Text` component.
+      //            By doing that, we assume that user doesn't want to consume pre-defined font properties.
+      // Case one:  Nested `Text` has different `variant` that specified in parent. For example:
+      //              <Chip>
+      //                <Text variant="displayMedium">Nested</Text>
+      //              </Chip>
+      // Solution:  To address the following scenario, the code below overrides the `variant`
+      //            specified in a parent in favor of children's variant:
+      if (props.variant) {
+        font = theme.fonts[props.variant as VariantProp<typeof props.variant>];
+        textStyle = [style, font];
+      }
+
+      // Case two:  Nested `Text` has specified `styles` which intefere
+      //            with font properties, from the parent's `variant`. For example:
+      //              <Chip>
+      //                <Text style={{fontSize: 30}}>Nested</Text>
+      //              </Chip>
+      // Solution:  To address the following scenario, the code below overrides the
+      //            parent's style with children's style:
+      if (!props.variant) {
+        textStyle = [style, props.style];
+      }
+    }
+
     if (typeof font !== 'object') {
       throw new Error(
         `Variant ${variant} was not provided properly. Valid variants are ${Object.keys(
@@ -109,10 +145,9 @@ const Text = (
       <NativeText
         ref={root}
         style={[
-          font,
           styles.text,
           { writingDirection, color: theme.colors.onSurface },
-          style,
+          textStyle,
         ]}
         {...rest}
       />
