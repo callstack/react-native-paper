d9e160634db1d16a8c72d081dfc7fb846a16b75c
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-06-09 09:35:47 -0500

    chore: remove unnecessary additional condition for value

--- src/components/Button/utils.tsx
+++ src/components/Button/utils.tsx
@@ -257,7 +257,7 @@ export const getButtonTouchableRippleStyle = (
     ) as Array<keyof ViewStyleBorderRadiusStyles>
   ).forEach((key) => {
     const value = style[key as keyof ViewStyleBorderRadiusStyles];
-    if (typeof value !== 'undefined' && typeof value === 'number') {
+    if (typeof value === 'number') {
       // Only subtract borderWidth if value is greater than 0
       const radius = value > 0 ? value - borderWidth : 0;
       touchableRippleStyle[key as keyof ViewStyleBorderRadiusStyles] = radius;
