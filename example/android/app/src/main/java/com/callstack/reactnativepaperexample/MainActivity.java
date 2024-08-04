991bb3b904094db1ae14e17c547d9a1eff266458
Kamil Kedzierski <kamil.kedzierski94@gmail.com>
2024-08-04 15:18:58 +0200

    fix: fixed issues with build

--- example/android/app/src/main/java/com/callstack/reactnativepaperexample/MainActivity.java
+++ example/android/app/src/main/java/com/callstack/reactnativepaperexample/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 import expo.modules.ReactActivityDelegateWrapper;
--- example/android/app/src/main/java/com/callstack/reactnativepaperexample/MainApplication.java
+++ example/android/app/src/main/java/com/callstack/reactnativepaperexample/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.flipper.ReactNativeFlipper;
 import com.facebook.soloader.SoLoader;
 
 import expo.modules.ApplicationLifecycleDispatcher;
