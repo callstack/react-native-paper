e0b4ec9a49379a8ccff63b61462a30f912a06c08
lukewalczak <lukasz.walczak.pwr@gmail.com>
2025-05-06 13:53:42 +0200

    chore: exclude hitslop from the list

--- scripts/typescript-output-lint.js
+++ scripts/typescript-output-lint.js
@@ -25,7 +25,6 @@ const unusedViewProps = [
   'onAccessibilityTap',
   'onMagicTap',
   'accessibilityIgnoresInvertColors',
-  'hitSlop',
   'removeClippedSubviews',
   'collapsable',
   'needsOffscreenAlphaCompositing',
