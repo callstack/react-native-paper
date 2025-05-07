b4656ffb6fef50537de8714e7609c3f521607d04
lukewalczak <lukasz.walczak.pwr@gmail.com>
2025-05-07 12:26:18 +0200

    chore: setup monorepo

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,55 +1,9 @@
-// /* eslint-disable import/no-commonjs */
-
 const { getDefaultConfig } = require('@expo/metro-config');
-const escape = require('escape-string-regexp');
-const blacklist = require('metro-config/src/defaults/exclusionList');
 const path = require('path');
+const { withMetroConfig } = require('react-native-monorepo-config');
 
-const pak = require('../package.json');
-
-const root = path.resolve(__dirname, '..');
-
-const defaultConfig = getDefaultConfig(__dirname);
-
-const modules = [
-  '@react-native-vector-icons/material-design-icons',
-  '@expo/vector-icons',
-  'expo-constants',
-  ...Object.keys(pak.peerDependencies),
-  '@react-navigation/native',
-  '@react-native/assets-registry',
-];
-
-module.exports = {
-  ...defaultConfig,
-
-  projectRoot: __dirname,
-  watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
-  resolver: {
-    ...defaultConfig.resolver,
-    blacklistRE: blacklist(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
-
-  transformer: {
-    ...defaultConfig.transformer,
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+/** @type {import('metro-config').MetroConfig} */
+module.exports = withMetroConfig(getDefaultConfig(__dirname), {
+  root: path.resolve(__dirname, '..'),
+  dirname: __dirname,
+});
