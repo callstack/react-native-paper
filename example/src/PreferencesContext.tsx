615ec2e52b8882fe18ad924c69809e87efe1b1a2
lukewalczak <lukasz.walczak.pwr@gmail.com>
2025-05-07 15:19:53 +0200

    fix: web example

--- example/src/PreferencesContext.tsx
+++ example/src/PreferencesContext.tsx
@@ -3,17 +3,17 @@ import * as React from 'react';
 import { MD2Theme, MD3Theme } from 'react-native-paper';
 
 export const PreferencesContext = React.createContext<{
-  toggleShouldUseDeviceColors?: () => void;
   toggleTheme: () => void;
   toggleRtl: () => void;
   toggleThemeVersion: () => void;
   toggleCollapsed: () => void;
   toggleCustomFont: () => void;
   toggleRippleEffect: () => void;
+  toggleShouldUseDeviceColors?: () => void;
+  theme: MD2Theme | MD3Theme;
+  rtl: boolean;
+  collapsed: boolean;
   customFontLoaded: boolean;
   rippleEffectEnabled: boolean;
-  collapsed: boolean;
-  rtl: boolean;
-  theme: MD2Theme | MD3Theme;
   shouldUseDeviceColors?: boolean;
 } | null>(null);
