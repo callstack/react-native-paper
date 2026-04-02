31c7a90ba3b2cde727cc9a1a407d6f87dde61295
Adrian Cotfas <adrcotfas@gmail.com>
2026-04-02 12:47:38 +0300

    refactor: remove usage of expo-material3-theme

--- src/styles/themes/DynamicTheme.android.tsx
+++ src/styles/themes/DynamicTheme.android.tsx
@@ -1,8 +1,8 @@
 import { Platform, PlatformColor } from 'react-native';
 
-import { DarkTheme } from './DarkTheme';
-import { LightTheme } from './LightTheme';
-import type { Theme } from '../../types';
+import { MD3DarkTheme } from './v3/DarkTheme';
+import { MD3LightTheme } from './v3/LightTheme';
+import type { MD3Theme } from '../../types';
 
 const isApi34 = (Platform.Version as number) >= 34;
 const isApi31 = (Platform.Version as number) >= 31;
@@ -29,232 +29,232 @@ const lightColors = {
   primary: pick(
     'system_primary_light',
     'system_accent1_600',
-    LightTheme.colors.primary
+    MD3LightTheme.colors.primary
   ),
   onPrimary: pick(
     'system_on_primary_light',
     'system_accent1_0',
-    LightTheme.colors.onPrimary
+    MD3LightTheme.colors.onPrimary
   ),
   primaryContainer: pick(
     'system_primary_container_light',
     'system_accent1_100',
-    LightTheme.colors.primaryContainer
+    MD3LightTheme.colors.primaryContainer
   ),
   onPrimaryContainer: pick(
     'system_on_primary_container_light',
     'system_accent1_900',
-    LightTheme.colors.onPrimaryContainer
+    MD3LightTheme.colors.onPrimaryContainer
   ),
   inversePrimary: pick(
     'system_primary_dark',
     'system_accent1_200',
-    LightTheme.colors.inversePrimary
+    MD3LightTheme.colors.inversePrimary
   ),
   secondary: pick(
     'system_secondary_light',
     'system_accent2_600',
-    LightTheme.colors.secondary
+    MD3LightTheme.colors.secondary
   ),
   onSecondary: pick(
     'system_on_secondary_light',
     'system_accent2_0',
-    LightTheme.colors.onSecondary
+    MD3LightTheme.colors.onSecondary
   ),
   secondaryContainer: pick(
     'system_secondary_container_light',
     'system_accent2_100',
-    LightTheme.colors.secondaryContainer
+    MD3LightTheme.colors.secondaryContainer
   ),
   onSecondaryContainer: pick(
     'system_on_secondary_container_light',
     'system_accent2_900',
-    LightTheme.colors.onSecondaryContainer
+    MD3LightTheme.colors.onSecondaryContainer
   ),
   tertiary: pick(
     'system_tertiary_light',
     'system_accent3_600',
-    LightTheme.colors.tertiary
+    MD3LightTheme.colors.tertiary
   ),
   onTertiary: pick(
     'system_on_tertiary_light',
     'system_accent3_0',
-    LightTheme.colors.onTertiary
+    MD3LightTheme.colors.onTertiary
   ),
   tertiaryContainer: pick(
     'system_tertiary_container_light',
     'system_accent3_100',
-    LightTheme.colors.tertiaryContainer
+    MD3LightTheme.colors.tertiaryContainer
   ),
   onTertiaryContainer: pick(
     'system_on_tertiary_container_light',
     'system_accent3_900',
-    LightTheme.colors.onTertiaryContainer
+    MD3LightTheme.colors.onTertiaryContainer
   ),
   error: pick(
     'system_error_light',
-    LightTheme.colors.error,
-    LightTheme.colors.error
+    MD3LightTheme.colors.error,
+    MD3LightTheme.colors.error
   ),
   onError: pick(
     'system_on_error_light',
-    LightTheme.colors.onError,
-    LightTheme.colors.onError
+    MD3LightTheme.colors.onError,
+    MD3LightTheme.colors.onError
   ),
   errorContainer: pick(
     'system_error_container_light',
-    LightTheme.colors.errorContainer,
-    LightTheme.colors.errorContainer
+    MD3LightTheme.colors.errorContainer,
+    MD3LightTheme.colors.errorContainer
   ),
   onErrorContainer: pick(
     'system_on_error_container_light',
-    LightTheme.colors.onErrorContainer,
-    LightTheme.colors.onErrorContainer
+    MD3LightTheme.colors.onErrorContainer,
+    MD3LightTheme.colors.onErrorContainer
   ),
   onSurface: pick(
     'system_on_surface_light',
     'system_neutral1_900',
-    LightTheme.colors.onSurface
+    MD3LightTheme.colors.onSurface
   ),
   onBackground: pick(
     'system_on_background_light',
     'system_neutral1_900',
-    LightTheme.colors.onBackground
+    MD3LightTheme.colors.onBackground
   ),
   onSurfaceVariant: pick(
     'system_on_surface_variant_light',
     'system_neutral2_700',
-    LightTheme.colors.onSurfaceVariant
+    MD3LightTheme.colors.onSurfaceVariant
   ),
   outline: pick(
     'system_outline_light',
     'system_neutral2_500',
-    LightTheme.colors.outline
+    MD3LightTheme.colors.outline
   ),
   outlineVariant: pick(
     'system_outline_variant_light',
     'system_neutral2_200',
-    LightTheme.colors.outlineVariant
+    MD3LightTheme.colors.outlineVariant
   ),
   inverseSurface: pick(
     'system_surface_dark',
     'system_neutral1_800',
-    LightTheme.colors.inverseSurface
+    MD3LightTheme.colors.inverseSurface
   ),
   inverseOnSurface: pick(
     'system_on_surface_dark',
     'system_neutral1_50',
-    LightTheme.colors.inverseOnSurface
+    MD3LightTheme.colors.inverseOnSurface
   ),
   surfaceContainerLowest: pick(
     'system_surface_container_lowest_light',
     'system_neutral2_0',
-    LightTheme.colors.surfaceContainerLowest
+    MD3LightTheme.colors.surfaceContainerLowest
   ),
   surfaceContainerLow: pick(
     'system_surface_container_low_light',
-    LightTheme.colors.surfaceContainerLow,
-    LightTheme.colors.surfaceContainerLow
+    MD3LightTheme.colors.surfaceContainerLow,
+    MD3LightTheme.colors.surfaceContainerLow
   ),
   surfaceContainerHighest: pick(
     'system_surface_container_highest_light',
     'system_neutral2_100',
-    LightTheme.colors.surfaceContainerHighest
+    MD3LightTheme.colors.surfaceContainerHighest
   ),
   surface: pick(
     'system_surface_light',
-    LightTheme.colors.surface,
-    LightTheme.colors.surface
+    MD3LightTheme.colors.surface,
+    MD3LightTheme.colors.surface
   ),
   surfaceDim: pick(
     'system_surface_dim_light',
-    LightTheme.colors.surfaceDim,
-    LightTheme.colors.surfaceDim
+    MD3LightTheme.colors.surfaceDim,
+    MD3LightTheme.colors.surfaceDim
   ),
   surfaceBright: pick(
     'system_surface_bright_light',
-    LightTheme.colors.surfaceBright,
-    LightTheme.colors.surfaceBright
+    MD3LightTheme.colors.surfaceBright,
+    MD3LightTheme.colors.surfaceBright
   ),
   surfaceContainer: pick(
     'system_surface_container_light',
-    LightTheme.colors.surfaceContainer,
-    LightTheme.colors.surfaceContainer
+    MD3LightTheme.colors.surfaceContainer,
+    MD3LightTheme.colors.surfaceContainer
   ),
   surfaceContainerHigh: pick(
     'system_surface_container_high_light',
-    LightTheme.colors.surfaceContainerHigh,
-    LightTheme.colors.surfaceContainerHigh
+    MD3LightTheme.colors.surfaceContainerHigh,
+    MD3LightTheme.colors.surfaceContainerHigh
   ),
   background: pick(
     'system_background_light',
-    LightTheme.colors.background,
-    LightTheme.colors.background
+    MD3LightTheme.colors.background,
+    MD3LightTheme.colors.background
   ),
   surfaceVariant: pick(
     'system_surface_variant_light',
-    LightTheme.colors.surfaceVariant,
-    LightTheme.colors.surfaceVariant
+    MD3LightTheme.colors.surfaceVariant,
+    MD3LightTheme.colors.surfaceVariant
   ),
   primaryFixed: pick(
     'system_primary_fixed',
     'system_accent1_100',
-    LightTheme.colors.primaryFixed
+    MD3LightTheme.colors.primaryFixed
   ),
   primaryFixedDim: pick(
     'system_primary_fixed_dim',
     'system_accent1_200',
-    LightTheme.colors.primaryFixedDim
+    MD3LightTheme.colors.primaryFixedDim
   ),
   onPrimaryFixed: pick(
     'system_on_primary_fixed',
     'system_accent1_900',
-    LightTheme.colors.onPrimaryFixed
+    MD3LightTheme.colors.onPrimaryFixed
   ),
   onPrimaryFixedVariant: pick(
     'system_on_primary_fixed_variant',
     'system_accent1_700',
-    LightTheme.colors.onPrimaryFixedVariant
+    MD3LightTheme.colors.onPrimaryFixedVariant
   ),
   secondaryFixed: pick(
     'system_secondary_fixed',
     'system_accent2_100',
-    LightTheme.colors.secondaryFixed
+    MD3LightTheme.colors.secondaryFixed
   ),
   secondaryFixedDim: pick(
     'system_secondary_fixed_dim',
     'system_accent2_200',
-    LightTheme.colors.secondaryFixedDim
+    MD3LightTheme.colors.secondaryFixedDim
   ),
   onSecondaryFixed: pick(
     'system_on_secondary_fixed',
     'system_accent2_900',
-    LightTheme.colors.onSecondaryFixed
+    MD3LightTheme.colors.onSecondaryFixed
   ),
   onSecondaryFixedVariant: pick(
     'system_on_secondary_fixed_variant',
     'system_accent2_700',
-    LightTheme.colors.onSecondaryFixedVariant
+    MD3LightTheme.colors.onSecondaryFixedVariant
   ),
   tertiaryFixed: pick(
     'system_tertiary_fixed',
     'system_accent3_100',
-    LightTheme.colors.tertiaryFixed
+    MD3LightTheme.colors.tertiaryFixed
   ),
   tertiaryFixedDim: pick(
     'system_tertiary_fixed_dim',
     'system_accent3_200',
-    LightTheme.colors.tertiaryFixedDim
+    MD3LightTheme.colors.tertiaryFixedDim
   ),
   onTertiaryFixed: pick(
     'system_on_tertiary_fixed',
     'system_accent3_900',
-    LightTheme.colors.onTertiaryFixed
+    MD3LightTheme.colors.onTertiaryFixed
   ),
   onTertiaryFixedVariant: pick(
     'system_on_tertiary_fixed_variant',
     'system_accent3_700',
-    LightTheme.colors.onTertiaryFixedVariant
+    MD3LightTheme.colors.onTertiaryFixedVariant
   ),
 };
 
@@ -262,241 +262,241 @@ const darkColors = {
   primary: pick(
     'system_primary_dark',
     'system_accent1_200',
-    DarkTheme.colors.primary
+    MD3DarkTheme.colors.primary
   ),
   onPrimary: pick(
     'system_on_primary_dark',
     'system_accent1_800',
-    DarkTheme.colors.onPrimary
+    MD3DarkTheme.colors.onPrimary
   ),
   primaryContainer: pick(
     'system_primary_container_dark',
     'system_accent1_700',
-    DarkTheme.colors.primaryContainer
+    MD3DarkTheme.colors.primaryContainer
   ),
   onPrimaryContainer: pick(
     'system_on_primary_container_dark',
     'system_accent1_100',
-    DarkTheme.colors.onPrimaryContainer
+    MD3DarkTheme.colors.onPrimaryContainer
   ),
   inversePrimary: pick(
     'system_primary_light',
     'system_accent1_600',
-    DarkTheme.colors.inversePrimary
+    MD3DarkTheme.colors.inversePrimary
   ),
   secondary: pick(
     'system_secondary_dark',
     'system_accent2_200',
-    DarkTheme.colors.secondary
+    MD3DarkTheme.colors.secondary
   ),
   onSecondary: pick(
     'system_on_secondary_dark',
     'system_accent2_800',
-    DarkTheme.colors.onSecondary
+    MD3DarkTheme.colors.onSecondary
   ),
   secondaryContainer: pick(
     'system_secondary_container_dark',
     'system_accent2_700',
-    DarkTheme.colors.secondaryContainer
+    MD3DarkTheme.colors.secondaryContainer
   ),
   onSecondaryContainer: pick(
     'system_on_secondary_container_dark',
     'system_accent2_100',
-    DarkTheme.colors.onSecondaryContainer
+    MD3DarkTheme.colors.onSecondaryContainer
   ),
   tertiary: pick(
     'system_tertiary_dark',
     'system_accent3_200',
-    DarkTheme.colors.tertiary
+    MD3DarkTheme.colors.tertiary
   ),
   onTertiary: pick(
     'system_on_tertiary_dark',
     'system_accent3_800',
-    DarkTheme.colors.onTertiary
+    MD3DarkTheme.colors.onTertiary
   ),
   tertiaryContainer: pick(
     'system_tertiary_container_dark',
     'system_accent3_700',
-    DarkTheme.colors.tertiaryContainer
+    MD3DarkTheme.colors.tertiaryContainer
   ),
   onTertiaryContainer: pick(
     'system_on_tertiary_container_dark',
     'system_accent3_100',
-    DarkTheme.colors.onTertiaryContainer
+    MD3DarkTheme.colors.onTertiaryContainer
   ),
   error: pick(
     'system_error_dark',
-    DarkTheme.colors.error,
-    DarkTheme.colors.error
+    MD3DarkTheme.colors.error,
+    MD3DarkTheme.colors.error
   ),
   onError: pick(
     'system_on_error_dark',
-    DarkTheme.colors.onError,
-    DarkTheme.colors.onError
+    MD3DarkTheme.colors.onError,
+    MD3DarkTheme.colors.onError
   ),
   errorContainer: pick(
     'system_error_container_dark',
-    DarkTheme.colors.errorContainer,
-    DarkTheme.colors.errorContainer
+    MD3DarkTheme.colors.errorContainer,
+    MD3DarkTheme.colors.errorContainer
   ),
   onErrorContainer: pick(
     'system_on_error_container_dark',
-    DarkTheme.colors.onErrorContainer,
-    DarkTheme.colors.onErrorContainer
+    MD3DarkTheme.colors.onErrorContainer,
+    MD3DarkTheme.colors.onErrorContainer
   ),
   onSurface: pick(
     'system_on_surface_dark',
     'system_neutral1_100',
-    DarkTheme.colors.onSurface
+    MD3DarkTheme.colors.onSurface
   ),
   onBackground: pick(
     'system_on_background_dark',
     'system_neutral1_100',
-    DarkTheme.colors.onBackground
+    MD3DarkTheme.colors.onBackground
   ),
   onSurfaceVariant: pick(
     'system_on_surface_variant_dark',
     'system_neutral2_200',
-    DarkTheme.colors.onSurfaceVariant
+    MD3DarkTheme.colors.onSurfaceVariant
   ),
   outline: pick(
     'system_outline_dark',
     'system_neutral2_400',
-    DarkTheme.colors.outline
+    MD3DarkTheme.colors.outline
   ),
   outlineVariant: pick(
     'system_outline_variant_dark',
     'system_neutral2_700',
-    DarkTheme.colors.outlineVariant
+    MD3DarkTheme.colors.outlineVariant
   ),
   inverseSurface: pick(
     'system_surface_light',
     'system_neutral1_100',
-    DarkTheme.colors.inverseSurface
+    MD3DarkTheme.colors.inverseSurface
   ),
   inverseOnSurface: pick(
     'system_on_surface_light',
     'system_neutral1_800',
-    DarkTheme.colors.inverseOnSurface
+    MD3DarkTheme.colors.inverseOnSurface
   ),
   surfaceContainerLowest: pick(
     'system_surface_container_lowest_dark',
-    DarkTheme.colors.surfaceContainerLowest,
-    DarkTheme.colors.surfaceContainerLowest
+    MD3DarkTheme.colors.surfaceContainerLowest,
+    MD3DarkTheme.colors.surfaceContainerLowest
   ),
   surfaceContainerLow: pick(
     'system_surface_container_low_dark',
     'system_neutral2_900',
-    DarkTheme.colors.surfaceContainerLow
+    MD3DarkTheme.colors.surfaceContainerLow
   ),
   surfaceContainerHighest: pick(
     'system_surface_container_highest_dark',
-    DarkTheme.colors.surfaceContainerHighest,
-    DarkTheme.colors.surfaceContainerHighest
+    MD3DarkTheme.colors.surfaceContainerHighest,
+    MD3DarkTheme.colors.surfaceContainerHighest
   ),
   surface: pick(
     'system_surface_dark',
-    DarkTheme.colors.surface,
-    DarkTheme.colors.surface
+    MD3DarkTheme.colors.surface,
+    MD3DarkTheme.colors.surface
   ),
   surfaceDim: pick(
     'system_surface_dim_dark',
-    DarkTheme.colors.surfaceDim,
-    DarkTheme.colors.surfaceDim
+    MD3DarkTheme.colors.surfaceDim,
+    MD3DarkTheme.colors.surfaceDim
   ),
   surfaceBright: pick(
     'system_surface_bright_dark',
-    DarkTheme.colors.surfaceBright,
-    DarkTheme.colors.surfaceBright
+    MD3DarkTheme.colors.surfaceBright,
+    MD3DarkTheme.colors.surfaceBright
   ),
   surfaceContainer: pick(
     'system_surface_container_dark',
-    DarkTheme.colors.surfaceContainer,
-    DarkTheme.colors.surfaceContainer
+    MD3DarkTheme.colors.surfaceContainer,
+    MD3DarkTheme.colors.surfaceContainer
   ),
   surfaceContainerHigh: pick(
     'system_surface_container_high_dark',
-    DarkTheme.colors.surfaceContainerHigh,
-    DarkTheme.colors.surfaceContainerHigh
+    MD3DarkTheme.colors.surfaceContainerHigh,
+    MD3DarkTheme.colors.surfaceContainerHigh
   ),
   background: pick(
     'system_background_dark',
-    DarkTheme.colors.background,
-    DarkTheme.colors.background
+    MD3DarkTheme.colors.background,
+    MD3DarkTheme.colors.background
   ),
   surfaceVariant: pick(
     'system_surface_variant_dark',
-    DarkTheme.colors.surfaceVariant,
-    DarkTheme.colors.surfaceVariant
+    MD3DarkTheme.colors.surfaceVariant,
+    MD3DarkTheme.colors.surfaceVariant
   ),
   primaryFixed: pick(
     'system_primary_fixed',
     'system_accent1_100',
-    DarkTheme.colors.primaryFixed
+    MD3DarkTheme.colors.primaryFixed
   ),
   primaryFixedDim: pick(
     'system_primary_fixed_dim',
     'system_accent1_200',
-    DarkTheme.colors.primaryFixedDim
+    MD3DarkTheme.colors.primaryFixedDim
   ),
   onPrimaryFixed: pick(
     'system_on_primary_fixed',
     'system_accent1_900',
-    DarkTheme.colors.onPrimaryFixed
+    MD3DarkTheme.colors.onPrimaryFixed
   ),
   onPrimaryFixedVariant: pick(
     'system_on_primary_fixed_variant',
     'system_accent1_700',
-    DarkTheme.colors.onPrimaryFixedVariant
+    MD3DarkTheme.colors.onPrimaryFixedVariant
   ),
   secondaryFixed: pick(
     'system_secondary_fixed',
     'system_accent2_100',
-    DarkTheme.colors.secondaryFixed
+    MD3DarkTheme.colors.secondaryFixed
   ),
   secondaryFixedDim: pick(
     'system_secondary_fixed_dim',
     'system_accent2_200',
-    DarkTheme.colors.secondaryFixedDim
+    MD3DarkTheme.colors.secondaryFixedDim
   ),
   onSecondaryFixed: pick(
     'system_on_secondary_fixed',
     'system_accent2_900',
-    DarkTheme.colors.onSecondaryFixed
+    MD3DarkTheme.colors.onSecondaryFixed
   ),
   onSecondaryFixedVariant: pick(
     'system_on_secondary_fixed_variant',
     'system_accent2_700',
-    DarkTheme.colors.onSecondaryFixedVariant
+    MD3DarkTheme.colors.onSecondaryFixedVariant
   ),
   tertiaryFixed: pick(
     'system_tertiary_fixed',
     'system_accent3_100',
-    DarkTheme.colors.tertiaryFixed
+    MD3DarkTheme.colors.tertiaryFixed
   ),
   tertiaryFixedDim: pick(
     'system_tertiary_fixed_dim',
     'system_accent3_200',
-    DarkTheme.colors.tertiaryFixedDim
+    MD3DarkTheme.colors.tertiaryFixedDim
   ),
   onTertiaryFixed: pick(
     'system_on_tertiary_fixed',
     'system_accent3_900',
-    DarkTheme.colors.onTertiaryFixed
+    MD3DarkTheme.colors.onTertiaryFixed
   ),
   onTertiaryFixedVariant: pick(
     'system_on_tertiary_fixed_variant',
     'system_accent3_700',
-    DarkTheme.colors.onTertiaryFixedVariant
+    MD3DarkTheme.colors.onTertiaryFixedVariant
   ),
 };
 
-export const DynamicLightTheme: Theme = {
-  ...LightTheme,
-  colors: { ...LightTheme.colors, ...lightColors },
+export const DynamicLightTheme: MD3Theme = {
+  ...MD3LightTheme,
+  colors: { ...MD3LightTheme.colors, ...lightColors },
 };
 
-export const DynamicDarkTheme: Theme = {
-  ...DarkTheme,
-  colors: { ...DarkTheme.colors, ...darkColors },
+export const DynamicDarkTheme: MD3Theme = {
+  ...MD3DarkTheme,
+  colors: { ...MD3DarkTheme.colors, ...darkColors },
 };
