7398600004bb15f981018d049bda631ab74e46c5
Francesco Clementi <francesco.clementi@elleffetech.com>
2026-05-07 16:22:53 +0200

    Refactor underlay style to use absoluteFill

--- src/components/TouchableRipple/TouchableRipple.native.tsx
+++ src/components/TouchableRipple/TouchableRipple.native.tsx
@@ -135,13 +135,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   underlay: {
-    ...(StyleSheet.absoluteFillObject || {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-    }),
+    ...StyleSheet.absoluteFill,
     zIndex: 2,
   },
 });
