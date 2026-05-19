de574b5d7542b431a352222f6ab63e08532a109c
Adrian Cotfas <adrcotfas@gmail.com>
2026-05-19 14:25:12 +0300

    feat: modernize Switch to MD3
    * BREAKING CHANGE: Switch props no longer extend native Switch. color, ios_backgroundColor, trackColor, thumbColor etc. are removed. Use the theme prop for color customization.

--- src/components/__tests__/Switch.test.tsx
+++ src/components/__tests__/Switch.test.tsx
@@ -1,224 +1,67 @@
 import * as React from 'react';
-import { Platform } from 'react-native';
 
-import color from 'color';
-
-import { getTheme } from '../../core/theming';
-import { render } from '../../test-utils';
-import {
-  white,
-  black,
-  grey400,
-  grey50,
-  grey800,
-  pink500,
-  grey700,
-} from '../../theme/colors';
+import { fireEvent, render } from '../../test-utils';
 import Switch from '../Switch/Switch';
-import { getSwitchColor } from '../Switch/utils';
 
-it('renders on switch', () => {
-  const tree = render(<Switch value />).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders off switch', () => {
-  const tree = render(<Switch value={false} />).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders disabled switch', () => {
-  const tree = render(<Switch disabled value />).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders switch with color', () => {
-  const tree = render(<Switch value color={pink500} />).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-describe('getSwitchColor - checked color', () => {
-  it('should return custom color', () => {
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-        color: 'purple',
-      })
-    ).toMatchObject({
-      checkedColor: 'purple',
-    });
+describe('Switch render', () => {
+  it('renders on', () => {
+    expect(render(<Switch value />).toJSON()).toMatchSnapshot();
   });
 
-  it('should return theme color, for theme version 3', () => {
+  it('renders off', () => {
+    expect(render(<Switch value={false} />).toJSON()).toMatchSnapshot();
+  });
+
+  it('renders disabled on', () => {
+    expect(render(<Switch disabled value />).toJSON()).toMatchSnapshot();
+  });
+
+  it('renders disabled off', () => {
     expect(
-      getSwitchColor({
-        theme: getTheme(),
-      })
-    ).toMatchObject({
-      checkedColor: getTheme().colors.primary,
-    });
+      render(<Switch disabled value={false} />).toJSON()
+    ).toMatchSnapshot();
+  });
+
+  it('renders with checked icon', () => {
+    expect(
+      render(<Switch value checkedIcon="check" />).toJSON()
+    ).toMatchSnapshot();
+  });
+
+  it('renders with per-state icons', () => {
+    expect(
+      render(
+        <Switch value checkedIcon="check" uncheckedIcon="close" />
+      ).toJSON()
+    ).toMatchSnapshot();
   });
 });
 
-describe('getSwitchColor - thumb tint color', () => {
-  it('should return undefined for iOS platform', () => {
-    Platform.OS = 'ios';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(true),
-      })
-    ).toMatchObject({
-      thumbTintColor: undefined,
-    });
+describe('Switch interaction', () => {
+  it('toggles to true when off and pressed', () => {
+    const onValueChange = jest.fn();
+    const { getByRole } = render(
+      <Switch value={false} onValueChange={onValueChange} />
+    );
+    fireEvent.press(getByRole('switch'));
+    expect(onValueChange).toHaveBeenCalledWith(true);
   });
 
-  it('should return correct disabled color, dark mode', () => {
-    Platform.OS = 'android';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(true),
-        disabled: true,
-      })
-    ).toMatchObject({
-      thumbTintColor: grey800,
-    });
+  it('toggles to false when on and pressed', () => {
+    const onValueChange = jest.fn();
+    const { getByRole } = render(
+      <Switch value onValueChange={onValueChange} />
+    );
+    fireEvent.press(getByRole('switch'));
+    expect(onValueChange).toHaveBeenCalledWith(false);
   });
 
-  it('should return correct disabled color, light mode', () => {
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-        disabled: true,
-      })
-    ).toMatchObject({
-      thumbTintColor: grey400,
-    });
-  });
-
-  it('should return correct checked color if there is value', () => {
-    Platform.OS = 'android';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-        value: true,
-        color: 'purple',
-      })
-    ).toMatchObject({
-      thumbTintColor: 'purple',
-    });
-  });
-
-  it('should return theme checked color, dark mode', () => {
-    Platform.OS = 'android';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(true),
-      })
-    ).toMatchObject({
-      thumbTintColor: grey400,
-    });
-  });
-
-  it('should return theme checked color, light mode', () => {
-    Platform.OS = 'android';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-      })
-    ).toMatchObject({
-      thumbTintColor: grey50,
-    });
-  });
-});
-
-describe('getSwitchColor - on tint color', () => {
-  it('should return checked color for iOS platform, for theme version 3', () => {
-    Platform.OS = 'ios';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-      })
-    ).toMatchObject({
-      onTintColor: getTheme().colors.primary,
-    });
-  });
-
-  it('should return custom color for iOS platform', () => {
-    Platform.OS = 'ios';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-        color: 'purple',
-      })
-    ).toMatchObject({
-      onTintColor: 'purple',
-    });
-  });
-
-  it('should return correct disabled color, for theme version 3, dark mode', () => {
-    Platform.OS = 'android';
-
-    expect(
-      getSwitchColor({
-        theme: getTheme(true),
-        disabled: true,
-      })
-    ).toMatchObject({
-      onTintColor: color(white).alpha(0.06).rgb().string(),
-    });
-  });
-
-  it('should return correct disabled color, light mode', () => {
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-        disabled: true,
-      })
-    ).toMatchObject({
-      onTintColor: color(black).alpha(0.12).rgb().string(),
-    });
-  });
-
-  it('should return correct checked color if there is value', () => {
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-        value: true,
-        color: 'purple',
-      })
-    ).toMatchObject({
-      checkedColor: 'purple',
-    });
-  });
-
-  it('should return theme checked color, dark mode', () => {
-    expect(
-      getSwitchColor({
-        theme: getTheme(true),
-      })
-    ).toMatchObject({
-      onTintColor: grey700,
-    });
-  });
-
-  it('should return theme checked color, light mode', () => {
-    expect(
-      getSwitchColor({
-        theme: getTheme(),
-      })
-    ).toMatchObject({
-      onTintColor: 'rgb(178, 175, 177)',
-    });
+  it('does not fire onValueChange when disabled', () => {
+    const onValueChange = jest.fn();
+    const { getByRole } = render(
+      <Switch value={false} disabled onValueChange={onValueChange} />
+    );
+    fireEvent.press(getByRole('switch'));
+    expect(onValueChange).not.toHaveBeenCalled();
   });
 });
