1f802b3c9a3aca9e92de719d7cf1e5c4928860ba
Adrian Cotfas <adrcotfas@gmail.com>
2026-05-28 16:42:32 +0300

    fix(docs): resolve docs build and icon issues
    - define `__DEV__` with webpack `DefinePlugin`
    - standardize docs icons on `@react-native-vector-icons/material-design-icons`
    - update docs examples and icon guidance
    - alias `@react-native-vector-icons/get-image` to `false`
    - flatten CSS nesting in `custom.css`
    - add `isDynamicColorSupported = false` to the non-Android `DynamicTheme` fallback
    - fix the `TypescaleKey` type re-export

--- docs/plugins/docusaurus-react-native-plugin.js
+++ docs/plugins/docusaurus-react-native-plugin.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = function () {
   return {
@@ -11,12 +12,15 @@ module.exports = function () {
             react: path.resolve('node_modules/react'),
             'react-native$': 'react-native-web',
             'react-native-paper': path.resolve('../src'),
-            'react-native-vector-icons/MaterialCommunityIcons': path.resolve(
-              'node_modules/@react-native-vector-icons/material-design-icons'
-            ),
+            '@react-native-vector-icons/get-image': false,
           },
           extensions: ['.web.js'],
         },
+        plugins: [
+          new webpack.DefinePlugin({
+            __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+          }),
+        ],
       };
     },
   };
